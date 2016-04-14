3599b5bb7d26b9a1ae8b35b7e5e5ea507ea69b2d
Takashi Matsuo <tmatsuo@google.com>
2016-04-14 15:57:41 -0700

    Run tests for Users API

--- appengine/standard/users/tests/e2e/bootstrap.php
+++ appengine/standard/users/tests/e2e/bootstrap.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../../../../testing/E2EDeploymentTrait.php';
-require_once __DIR__ . '/../../../../testing/GaeApp.php';
-require_once __DIR__ . '/../../../../testing/LocalTestTrait.php';
+require __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../../testing/E2EDeploymentTrait.php';
+require_once __DIR__ . '/../../../../../testing/GaeApp.php';
+require_once __DIR__ . '/../../../../../testing/LocalTestTrait.php';
