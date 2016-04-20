7925f929723e55dcc902916fe5732ab866b25f74
Terrence Ryan <tpryan@google.com>
2016-04-20 14:00:33 -0700

    Adding Examples and test code for sending mail from Apennine PHP

---
+++ appengine/standard/mail/test/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/mocks/Message.php';
---
+++ appengine/standard/mail/test/mailTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use google\appengine\api\mail\Message;
+
+class mailTest extends PHPUnit_Framework_TestCase
+{
+    public function testSendMail()
+    {
+
+        ob_start();
+        include __DIR__ . '/../index.php';
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertContains('Mail Sent', $result);
+    }
+
+    public function testIncomingHandle()
+    {
+
+        $_POST["content"] ="";
+
+        ob_start();
+        include __DIR__ . '/../handle_incoming_email.php';
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertContains("1", $result);
+    }
+    public function testBounceHandle()
+    {
+
+        $_POST["content"] ="";
+
+        ob_start();
+        include __DIR__ . '/../handle_bounced_email.php';
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertContains("1", $result);
+    }
+
+
+}
