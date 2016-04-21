e32bc7d8318352113e36d38f4258801c61ba0130
Jeffrey Rennie <rennie@google.com>
2016-04-21 14:06:26 -0700

    Migrate all the code samples in users/...
    https://cloud.google.com/appengine/docs/php/users/...

---
+++ appengine/standard/users/testing/E2EUsersTestTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Test;
+
+trait E2EUsersTestTrait
+{
+    public function testIndex()
+    {
+        // Access the modules app top page.
+        $resp = $this->client->get('');
+        $this->assertEquals('200', $resp->getStatusCode(),
+            'top page status code');
+        $this->assertContains(
+            'register',
+            $resp->getBody()->getContents());
+    }
+
+    public function testAdmin()
+    {
+        // Access the modules app top page.
+        $resp = $this->client->get('/admin');
+        $this->assertEquals('200', $resp->getStatusCode());
+        $this->assertContains(
+            'administrator',
+            $resp->getBody()->getContents());
+    }
+
+    public function testUser()
+    {
+        // Access the modules app top page.
+        $resp = $this->client->get('/user');
+        $this->assertEquals('200', $resp->getStatusCode());
+    }
+
+    public function testFederatedUser()
+    {
+        // Access the modules app top page.
+        $resp = $this->client->get('/federatedUser');
+        $this->assertEquals('200', $resp->getStatusCode());
+    }
+}
