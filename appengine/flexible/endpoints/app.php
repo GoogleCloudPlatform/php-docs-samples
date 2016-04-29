e18f82005513922425afba1764c36d8e3f3b576f
Brent Shaffer <betterbrent@google.com>
2016-04-29 14:09:46 -0700

    endpoints PHP example and tests (#95)

---
+++ appengine/flexible/endpoints/app.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Google Cloud Endpoints sample application.
+ *
+ * Demonstrates how to create a simple echo API as well as how to deal with
+ * various authentication methods.
+ */
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Yaml;
+
+// create the Silex application
+$app = new Application();
+$app->register(new TwigServiceProvider());
+$app['twig.path'] = [ __DIR__ ];
+
+$app->get('/', function () use ($app) {
+    /** @var Twig_Environment $twig */
+    $twig = $app['twig'];
+
+    // Shows the index page
+    return $app['twig']->render('endpoints.html.twig');
+});
+
+$app->get('/api-docs', function () use ($app) {
+    // Serves up the Swagger spec for the API.
+    $swaggerText = file_get_contents(__DIR__ . '/swagger.yaml');
+    $swaggerArray = Yaml::parse($swaggerText);
+
+    return $app->json($swaggerArray);
+});
+
+$app->post('/echo', function () use ($app) {
+    // Simple echo service.
+    $message = $app['request']->get('message');
+    return $app->json(['message' => $message]);
+});
+
+$app->get('/auth/info/googlejwt', function () use ($app) {
+    // Auth info with Google signed JWT.
+    return $app->json($app['auth_info']);
+});
+
+
+$app->get('/auth/info/googleidtoken', function () use ($app) {
+    // Auth info with Google ID token.
+    return $app->json($app['auth_info']);
+});
+
+$app['auth_info'] = function () use ($app) {
+    /** @var Symfony\Component\HttpFoundation\Request $request */
+    $request = $app['request'];
+    // Retrieves the authenication information from Google Cloud Endpoints.
+    $encoded_info = $request->headers->get('X-Endpoint-API-UserInfo');
+
+    if ($encoded_info) {
+        $info_json = utf8_decode(base64_decode($encoded_info));
+        $user_info = json_decode($info_json);
+    } else {
+        $user_info = ['id' => 'anonymous'];
+    }
+
+    return $user_info;
+};
+
+// Accept JSON requests
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+return $app;
---
+++ appengine/flexible/endpoints/test/EndpointsCommandTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Google\Cloud\Samples\Appengine\Endpoints\EndpointsCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class EndpointsCommandTest extends PHPUnit_Framework_TestCase
+{
+    private $host;
+    private $apiKey;
+    private $message;
+
+    public function setUp()
+    {
+        if (!$host = getenv('GOOGLE_ENDPOINTS_HOST')) {
+            return $this->markTestSkipped('Set the GOOGLE_ENDPOINTS_HOST environment variable');
+        }
+
+        if (!$api_key = getenv('GOOGLE_ENDPOINTS_APIKEY')) {
+            return $this->markTestSkipped('Set the GOOGLE_ENDPOINTS_APIKEY environment variable');
+        }
+
+        $this->host = $host;
+        $this->apiKey = $api_key;
+    }
+
+    public function testEndpointsCommandWithNoCredentials()
+    {
+        $command = new EndpointsCommand();
+        $tester = new CommandTester($command);
+        $message = <<<EOF
+So if you're lost and on your own
+You can never surrender
+And if your path won't lead you home
+You can never surrender
+EOF;
+        $input = [
+            'host' => $this->host,
+            'api_key' => $this->apiKey,
+            '--message' => $message,
+        ];
+
+        $result = $tester->execute($input);
+
+        $this->assertEquals(0, $result);
+        $jsonFlags = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
+        $this->assertContains(json_encode($message, $jsonFlags), $tester->getDisplay());
+    }
+
+    public function testEndpointsCommandWithApplicationCredentials()
+    {
+        if (!$creds = getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
+            return $this->markTestSkipped('Set the GOOGLE_APPLICATION_CREDENTIALS environment variable');
+        }
+
+        $command = new EndpointsCommand();
+        $tester = new CommandTester($command);
+        $arguments = [
+            'host' => $this->host,
+            'api_key' => $this->apiKey,
+            'credentials' => $creds,
+        ];
+        $options = [];
+
+        $result = $tester->execute($arguments, $options);
+
+        $this->assertEquals(0, $result);
+
+        $credentials = json_decode(file_get_contents($creds), true);
+        $this->assertContains('123456', $tester->getDisplay());
+    }
+
+    public function testEndpointsCommandWithClientSecrets()
+    {
+        if (!$creds = getenv('GOOGLE_CLIENT_SECRETS')) {
+            return $this->markTestSkipped('Set the GOOGLE_CLIENT_SECRETS environment variable');
+        }
+
+        $command = new EndpointsCommand();
+        $tester = new CommandTester($command);
+        $arguments = [
+            'host' => $this->host,
+            'api_key' => $this->apiKey,
+            'credentials' => $creds
+        ];
+        $options = [];
+
+        $result = $tester->execute($arguments, $options);
+
+        $this->assertEquals(0, $result);
+
+        $credentials = json_decode(file_get_contents($creds), true);
+        $this->assertContains('id', $tester->getDisplay());
+        $this->assertContains('email', $tester->getDisplay());
+    }
+}
