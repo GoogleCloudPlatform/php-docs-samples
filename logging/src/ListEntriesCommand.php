d300e0da63b6a3721e97b6eff54cd06cbcdc32d6
Takashi Matsuo <tmatsuo@google.com>
2016-09-08 15:51:24 -0700

    Adding ListEntries and DeleteLogger commands (#155)
    * Adding ListEntries and DeleteLogger commands

---
+++ logging/src/ListEntriesCommand.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\Logging;
+
+// [START list_log_use]
+use Google\Cloud\Logging\LoggingClient;
+// [END list_log_use]
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ListEntriesCommand
+ * @package Google\Cloud\Samples\Logging
+ *
+ * This command simply lists log messages.
+ */
+class ListEntriesCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('list-entries')
+            ->setDescription('Lists log entries in the logger');
+        $this->addCommonOptions();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $project = $input->getOption('project');
+        $loggerName = $input->getOption('logger');
+        // [START list_log]
+        $logging = new LoggingClient(['projectId' => $project]);
+        $loggerFullName = sprintf('projects/%s/logs/%s', $project, $loggerName);
+        $options = [
+            'filter' => sprintf('logName = "%s"', $loggerFullName)
+        ];
+        foreach ($logging->entries($options) as $entry) {
+            /* @var $entry \Google\Cloud\Logging\Entry */
+            printf(
+                "%s : %s\n",
+                $entry->info()['timestamp'],
+                $entry->info()['textPayload']
+            );
+        }
+        // [END list_log]
+    }
+}
---
+++ logging/test/ListEntriesCommandTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\Logging\Tests;
+
+use Google\Cloud\Samples\Logging\ListEntriesCommand;
+use Google\Cloud\Samples\Logging\WriteCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Functional tests for ListEntriesCommand.
+ */
+class ListEntriesCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /* @var $hasCredentials boolean */
+    protected static $hasCredentials;
+    /* @var $projectId mixed|string */
+    private $projectId;
+
+    public static function setUpBeforeClass()
+    {
+        $path = getenv('GOOGLE_APPLICATION_CREDENTIALS');
+        self::$hasCredentials = $path && file_exists($path) &&
+            filesize($path) > 0;
+    }
+
+    public function setUp()
+    {
+        if (!self::$hasCredentials) {
+            $this->markTestSkipped('No application credentials were found.');
+        }
+
+        if (!$this->projectId = getenv('GOOGLE_PROJECT_ID')) {
+            $this->markTestSkipped('No project ID');
+        }
+        $application = new Application();
+        $application->add(new WriteCommand());
+        $commandTester = new CommandTester($application->get('write'));
+        $commandTester->execute(
+            [
+                '--project' => $this->projectId,
+                '--logger' => 'my_test_logger',
+                'message' => 'Test Message'
+            ],
+            ['interactive' => false]
+        );
+        sleep(2);
+    }
+
+    public function testListEntries()
+    {
+        $application = new Application();
+        $application->add(new ListEntriesCommand());
+        $commandTester = new CommandTester($application->get('list-entries'));
+        $commandTester->execute(
+            ['--project' => $this->projectId, '--logger' => 'my_test_logger'],
+            ['interactive' => false]
+        );
+        $this->expectOutputRegex('/: Test Message/');
+    }
+}
