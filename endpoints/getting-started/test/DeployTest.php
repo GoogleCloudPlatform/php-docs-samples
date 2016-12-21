bef7b5207a0fe5a583c42e39aa0a293b6a33f1a1
Brent Shaffer <betterbrent@google.com>
2016-12-21 15:34:43 -0800

    fixes DeployTest for endpoints sample

--- endpoints/getting-started/test/DeployTest.php
+++ endpoints/getting-started/test/DeployTest.php
@@ -46,9 +46,16 @@ class DeployTest extends \PHPUnit_Framework_TestCase
         $clientId = getenv('GOOGLE_CLIENT_ID');
         $serviceAccountEmail = getenv('GOOGLE_SERVICE_ACCOUNT_EMAIL');
         if (empty($clientId) || empty($serviceAccountEmail)) {
-            $this->markTestSkipped('Please set GOOGLE_CLIENT_ID, GOOGLE_PROJECT_ID '
+            self::markTestSkipped('Please set GOOGLE_CLIENT_ID, GOOGLE_PROJECT_ID '
                 . 'and GOOGLE_SERVICE_ACCOUNT_EMAIL');
         }
+        $serviceName = getenv('GOOGLE_ENDPOINTS_SERVICE_NAME');
+        $configId = getenv('GOOGLE_ENDPOINTS_CONFIG_ID');
+        if (empty($serviceName) || empty($configId)) {
+            self::markTestSkipped('Please set GOOGLE_ENDPOINTS_CONFIG_ID '
+                . 'and GOOGLE_ENDPOINTS_SERVICE_NAME');
+        }
+
         // copy the source files to a temp directory
         $tmpDir = FileUtil::cloneDirectoryIntoTmp(__DIR__ . '/..');
         self::$gcloudWrapper->setDir($tmpDir);
@@ -56,34 +63,20 @@ class DeployTest extends \PHPUnit_Framework_TestCase
         chdir($tmpDir);
 
         // update the swagger file for our configuration
-        $swagger = str_replace(
+        $openapiYaml = str_replace(
             ['YOUR-PROJECT-ID', 'YOUR-CLIENT-ID', 'YOUR-SERVICE-ACCOUNT-EMAIL'],
             [self::getProjectId(), $clientId, $serviceAccountEmail],
             file_get_contents('openapi.yaml')
         );
-        file_put_contents($tmpDir . '/openapi.yaml', $swagger);
-    }
+        file_put_contents($tmpDir . '/openapi.yaml', $openapiYaml);
 
-    /**
-     * Override the "deployApp" method in the trait so that we can
-     * use "gcloud beta app deploy" instead of "gcloud app deploy"
-     *
-     * @beforeClass
-     */
-    public static function deployApp()
-    {
-        $projectId = self::getProjectId();
-        $versionId = self::getVersionId();
-        self::$gcloudWrapper = new GcloudWrapper($projectId, $versionId);
-        self::beforeDeploy();
-        $cmd = sprintf('gcloud -q beta app deploy --project %s --version %s --no-promote',
-            $projectId,
-            $versionId);
-        $timeout = 60 * 60; // timeout is 1 hour
-        self::executeWithRetry($cmd, $timeout);
-        if ((int) $delay = getenv('GOOGLE_DEPLOYMENT_DELAY')) {
-            sleep($delay);
-        }
+        // update app.yaml
+        $appYaml = str_replace(
+            ['ENDPOINTS SERVICE NAME', 'ENDPOINTS CONFIG ID'],
+            [$serviceName, $configId],
+            file_get_contents('app.yaml')
+        );
+        file_put_contents($tmpDir . '/app.yaml', $appYaml);
     }
 
     public function testEcho()
@@ -113,5 +106,23 @@ EOF;
         $this->assertNotNull($json);
         $this->assertArrayHasKey('message', $json);
         $this->assertEquals($message, $json['message']);
+    }
+
+    public function test302()
+    {
+        // create and send in JSON request
+        $response = $this->client->request(
+            'POST',
+            '/echo',
+            ['exceptions' => false]
+        );
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $json = json_decode((string) $response->getBody(), true);
+        $this->assertArrayHasKey('message', $json);
+        $expectedString = 'Method doesn\'t allow unregistered callers ' .
+            '(callers without established identity). Please use API Key ' .
+            'or other form of API consumer identity to call this API.';
+        $this->assertEquals($expectedString, $json['message']);
     }
 }
