3a4a4905583a39b56b877b536132bb44a6e3c571
Brent Shaffer <betterbrent@google.com>
2017-01-12 13:09:35 -0800

    adds error reporting quickstart

---
+++ error_reporting/quickstart/quickstart.php
@@ -0,0 +1,39 @@
+<?php
+
+// Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
+# [START error_reporting]
+// Imports the Google Cloud client library
+use Google\Cloud\Logging\LoggingClient;
+
+// Your Google Cloud Platform project ID
+$projectId = 'YOUR_PROJECT_ID';
+
+// Instantiates a client
+$logging = new LoggingClient([
+    'projectId' => $projectId
+]);
+
+// The name of the log to write to
+$logName = 'my-log';
+
+// Selects the log to write to
+$logger = $logging->logger($logName);
+
+$handlerFunction = function(Exception $e) use ($logger) {
+    // Creates the log entry with the exception trace
+    $entry = $logger->entry([
+        'message' => $e->getTraceAsString(),
+    ]);
+    // Writes the log entry
+    $logger->write($entry);
+
+    print("Exception logged to Stack Driver Error Reporting" . PHP_EOL);
+};
+
+// Sets PHP's default exception handler
+set_exception_handler($handlerFunction);
+
+throw new Exception('This will be logged to Stack Driver Error Reporting');
+# [END error_reporting]
