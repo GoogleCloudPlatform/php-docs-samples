d224d7bd99d4caa074150534f54d23d25205b549
Brent Shaffer <betterbrent@google.com>
2017-01-20 13:47:14 -0800

    adds region tags for pubsub app

--- pubsub/app/index.php
+++ pubsub/app/index.php
@@ -22,8 +22,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/app.php';
 
 $app['project_id'] = getenv('GOOGLE_PROJECT_ID') ?: getenv('GCLOUD_PROJECT');
+# [START pubsub_variables]
 $app['topic'] = 'php-example-topic';
 $app['subscription'] = 'php-example-subscription';
+# [END pubsub_variables]
 $app['debug'] = true;
 
 $app->run();
