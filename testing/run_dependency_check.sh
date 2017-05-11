62bb9f7a0c84ba20c482669f0fa1e16c1f54277c
Brent Shaffer <betterbrent@google.com>
2017-05-11 16:40:42 -0700

    adds Makefile and test scripts

---
+++ testing/run_dependency_check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# Loop through all directories containing "phpunit.xml*" and run the test suites.
+find * -name 'phpunit.xml*' -not -path '*/vendor/*' -exec dirname {} \; | while read DIR
+do
+    pushd ${DIR}
+    if [ -f "composer.json" ]; then
+        # install composer dependencies
+        composer install
+        # verify direct google dependencies are up to date
+        if composer outdated --direct -m | grep -q 'google/' ; then
+            # output out-of-date libraries
+            echo "Some dependencies are out of date in \"$DIR\""
+            echo "run \"make dependency-update\" to update them"
+            composer outdated --direct -m --strict
+        fi
+    fi
+    popd
+done
