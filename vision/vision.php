6623942c15b0a9dd2f655d8198a9add5d4748fee
Ryan Matsumoto <ryanmats@google.com>
2017-07-28 17:10:53 -0700

    fixed minor linting issue

--- vision/vision.php
+++ vision/vision.php
@@ -22,8 +22,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputDefinition;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 # Includes the autoloader for libraries installed with composer
 require __DIR__ . '/vendor/autoload.php';
