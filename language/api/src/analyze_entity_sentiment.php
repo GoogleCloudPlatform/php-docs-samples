9fcad498d4c5311a032a33824f2ea7f008699073
Ryan Matsumoto <ryanmats@google.com>
2017-08-21 13:18:08 -0700

    Added AgentHeaderDescriptor to GRPC functions

--- language/api/src/analyze_entity_sentiment.php
+++ language/api/src/analyze_entity_sentiment.php
@@ -28,6 +28,7 @@ use Google\Cloud\Language\V1beta2\LanguageServiceClient;
 use Google\Cloud\Language\V1beta2\Document;
 use Google\Cloud\Language\V1beta2\Document_Type;
 use Google\Cloud\Language\V1beta2\EncodingType;
+use Google\GAX\AgentHeaderDescriptor;
 
 /**
  * Find the entities in text.
