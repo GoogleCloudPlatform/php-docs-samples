c622b9048b03599968c74b8a08393f4a3520fc28
Takashi Matsuo <tmatsuo@google.com>
2018-01-25 13:08:20 -0800

    CS fix

--- appengine/flexible/supervisord/addition/worker.php
+++ appengine/flexible/supervisord/addition/worker.php
@@ -23,8 +23,7 @@ $currentFile = '/tmp/currentQuote';
 
 set_time_limit(0);
 
-while(1)
-{
+while (1) {
     $quote = file_get_contents(
         'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
     );
