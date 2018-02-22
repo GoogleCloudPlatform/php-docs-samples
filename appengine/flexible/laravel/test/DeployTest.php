3c642da9c108a267c5b0996c4747df54cb96aeca
Takashi Matsuo <tmatsuo@google.com>
2018-02-21 17:14:29 -0800

    Add logging configuration to the laravel sample

--- appengine/flexible/laravel/test/DeployTest.php
+++ appengine/flexible/laravel/test/DeployTest.php
@@ -17,15 +17,19 @@
 
 namespace Google\Cloud\Samples\AppEngine\Laravel;
 
+use Google\Cloud\Logging\LoggingClient;
 use Google\Cloud\TestUtils\AppEngineDeploymentTrait;
+use Google\Cloud\TestUtils\EventuallyConsistentTestTrait;
 use Google\Cloud\TestUtils\ExecuteCommandTrait;
 use Google\Cloud\TestUtils\FileUtil;
 use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
 
-class DeployTest extends \PHPUnit_Framework_TestCase
+class DeployTest extends TestCase
 {
     use AppEngineDeploymentTrait;
     use ExecuteCommandTrait;
+    use EventuallyConsistentTestTrait;
 
     public static function beforeDeploy()
     {
@@ -52,6 +56,15 @@ class DeployTest extends \PHPUnit_Framework_TestCase
         $process = self::createProcess($cmd);
         $process->setTimeout(300); // 5 minutes
         self::executeProcess($process);
+        // add cloud libraries
+        $cmd = sprintf(
+            'composer --working-dir=%s require google/cloud-logging '
+            . 'google/cloud-error-reporting',
+            $targetDir
+        );
+        $process = self::createProcess($cmd);
+        $process->setTimeout(300); // 5 minutes
+        self::executeProcess($process);
 
         // copy in the app.yaml and add the app key.
         $appYaml = str_replace([
@@ -60,6 +73,19 @@ class DeployTest extends \PHPUnit_Framework_TestCase
             self::execute('php artisan key:generate --show --no-ansi'),
         ], file_get_contents(__DIR__ . '/../app.yaml'));
         file_put_contents($targetDir . '/app.yaml', $appYaml);
+        // move the code for the sample to the new laravel installation
+        mkdir("$targetDir/app/Logging", 0700, true);
+        $files = [
+            'routes/web.php',
+            'config/logging.php',
+            'app/Exceptions/Handler.php',
+            'app/Logging/CreateCustomLogger.php',
+        ];
+        foreach ($files as $file) {
+            $source = sprintf('%s/../%s', __DIR__, $file);
+            $target = sprintf('%s/%s', $targetDir, $file);
+            copy($source, $target);
+        }
     }
 
     private static function addPostDeployCommands($targetDir)
@@ -67,7 +93,6 @@ class DeployTest extends \PHPUnit_Framework_TestCase
         $contents = file_get_contents($targetDir . '/composer.json');
         $json = json_decode($contents, true);
         $json['scripts']['post-install-cmd'] = [
-            'chmod -R 755 bootstrap\/cache',
             'php artisan cache:clear',
         ];
         $newContents = json_encode($json, JSON_PRETTY_PRINT);
@@ -85,5 +110,79 @@ class DeployTest extends \PHPUnit_Framework_TestCase
         );
         $content = $resp->getBody()->getContents();
         $this->assertContains('Laravel', $content);
+    }
+
+    public function testNormalLog()
+    {
+        // Access a page erroring with 500
+        $token = uniqid();
+        // The routes are defined in routes/web.php
+        $path = "/log/$token";
+        $resp = $this->client->request('GET', $path, ['http_errors' => false]);
+        $this->assertEquals(
+            '200',
+            $resp->getStatusCode(),
+            'log page status code'
+        );
+        $logging = new LoggingClient(
+            ['projectId' => getenv('GOOGLE_PROJECT_ID')]
+        );
+        // 'app' is the default logname of our Stackdriver Logging
+        // integration.
+        $logger = $logging->logger('app');
+
+        $this->runEventuallyConsistentTest(
+            function () use ($logger, $token) {
+                $logs = $logger->entries([
+                    'pageSize' => 100,
+                    'orderBy' => 'timestamp desc',
+                    'resultLimit' => 100
+                ]);
+                $found = false;
+                foreach ($logs as $log) {
+                    $info = $log->info();
+                    if (strpos("token: $token", $info['jsonPayload']['message']) !== 0) {
+                        $found = true;
+                    }
+                }
+                $this->assertTrue($found, 'The log entry was not found');
+            });
+    }
+
+    public function testErrorLog()
+    {
+        // Access a page erroring with 500
+        $token = uniqid();
+        // The routes are defined in routes/web.php
+        $path = "/exception/$token";
+        $resp = $this->client->request('GET', $path, ['http_errors' => false]);
+        $this->assertEquals(
+            '500',
+            $resp->getStatusCode(),
+            'exception page status code'
+        );
+        $logging = new LoggingClient(
+            ['projectId' => getenv('GOOGLE_PROJECT_ID')]
+        );
+        // 'app-error' is the default logname of our Stackdriver Error
+        // Reporting integration.
+        $logger = $logging->logger('app-error');
+
+        $this->runEventuallyConsistentTest(
+            function () use ($logger, $token) {
+                $logs = $logger->entries([
+                    'pageSize' => 100,
+                    'orderBy' => 'timestamp desc',
+                    'resultLimit' => 100
+                ]);
+                $found = false;
+                foreach ($logs as $log) {
+                    $info = $log->info();
+                    if (strpos("token: $token", $info['jsonPayload']['message']) !== 0) {
+                        $found = true;
+                    }
+                }
+                $this->assertTrue($found, 'The log entry was not found');
+            });
     }
 }
