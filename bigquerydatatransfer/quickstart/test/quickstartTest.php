adc91bc76a09c6fc5584b507a213c8d4312dbb41
Tim Swast <swast@google.com>
2018-04-11 15:50:40 -0700

    BQ-DTS: Add quickstart
    Add quickstart for BigQuery Data Transfer API.
    
    This sample is intended to appear in the documentation at https://cloud.google.com/bigquery/transfer/docs/reference/libraries

---
+++ bigquerydatatransfer/quickstart/test/quickstartTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class quickstartTest extends PHPUnit_Framework_TestCase
+{
+    private $dataset;
+
+    public function testQuickstart()
+    {
+        if (!$projectId = getenv('GOOGLE_PROJECT_ID')) {
+            $this->markTestSkipped('GOOGLE_PROJECT_ID must be set.');
+        }
+
+        $datasetId = 'my_new_dataset_' . time();
+        $file = sys_get_temp_dir() . '/bigquerydatatransfer_quickstart.php';
+        $contents = file_get_contents(__DIR__ . '/../quickstart.php');
+        $contents = str_replace(
+            ['YOUR_PROJECT_ID', '__DIR__'],
+            [$projectId, sprintf('"%s/.."', __DIR__)],
+            $contents
+        );
+        file_put_contents($file, $contents);
+
+        // Invoke quickstart.php
+        $this->result = include $file;
+
+        // Make sure it looks correct
+        $this->assertTrue($this->result);
+    }
+}
