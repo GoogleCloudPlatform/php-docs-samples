291a6d2bd445e4ce04bfba1d7f870d488ed4b60f
Adam Ross <adamross@google.com>
2018-07-18 09:11:43 -0700

    Revert accidental commits.

--- appengine/php72/grpc/index.php
+++ appengine/php72/grpc/index.php
@@ -2,18 +2,21 @@
 
 // Static list provides security against URL injection by default.
 $routes = [
-  'spanner',
-  'monitoring',
-]
+    'spanner',
+    'monitoring',
+];
 
 // Keeping things fast for a small number of routes.
 $regex = '/\/(' . join($routes, '|') . ')\.php/';
-if (preg_match($regex, $_SERVER['REQUEST_URI'], $matches)) {
-  $file_path = '.' . $matches[0];
-  if (file_exists($file_path)) {
-    require($file_path);
-    return;
-  }
+if (preg_match($regex, $_SERVER['REQUEST_URI'], $matches))
+{
+    $file_path = __DIR__ . $matches[0];
+    if (file_exists($file_path))
+    {
+        require($file_path);
+        return;
+    }
+}
 
 // The homepage is the default behavior.
 // By checking for $_SERVER['REQUEST_URI'] == '/' this could be extended
