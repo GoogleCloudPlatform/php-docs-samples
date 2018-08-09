62f84148b595fc1c4dce00d906fe55b0f5a86fb8
Brent Shaffer <betterbrent@google.com>
2018-08-08 17:25:12 -0700

    better cloudsql samples for appengine php 7.2

--- appengine/php72/cloudsql/index.php
+++ appengine/php72/cloudsql/index.php
@@ -15,22 +15,12 @@
  * limitations under the License.
  */
 
-$dsn = getenv('CLOUDSQL_DSN');
-$user = getenv('CLOUDSQL_USER');
-$password = getenv('CLOUDSQL_PASSWORD');
-
 // Ensure the required environment variables are set to run the application
-if (!isset($dsn, $user) || false === $password) {
-    throw new Exception('Set CLOUDSQL_DSN, CLOUDSQL_USER, and CLOUDSQL_PASSWORD environment variables');
+if (!getenv('CLOUDSQL_DSN') || !getenv('CLOUDSQL_USER') || false === getenv('CLOUDSQL_PASSWORD')) {
+    die('Set CLOUDSQL_DSN, CLOUDSQL_USER, and CLOUDSQL_PASSWORD environment variables');
 }
 
 # [START gae_cloudsql_example]
-// Create the PDO object to talk to CloudSQL. Use the following variables:
-//
-//     $dsn = "mysql:dbname=DATABASE;unix_socket=/cloudsql/CONNECTION_NAME";
-//     $user = 'YOUR_CLOUDSQL_USER';
-//     $password = 'YOUR_CLOUDSQL_PASSWORD';
-//
 // If the unix socket is unavailable, try to connect using TCP. This will work
 // if you're running a local MySQL server or using the Cloud SQL proxy, for example:
 //
@@ -38,10 +28,16 @@ if (!isset($dsn, $user) || false === $password) {
 //
 // This will mean your DSN for connecting locally to Cloud SQL would look like this:
 //
+//     // for MySQL
 //     $dsn = "mysql:dbname=DATABASE;host=127.0.0.1";
+//     // for PostgreSQL
+//     $dsn = "pgsql:dbname=DATABASE;host=127.0.0.1";
 //
-// For PostgreSQL examples see `app-postgres.yaml`
-//
+$dsn = getenv('CLOUDSQL_DSN');
+$user = getenv('CLOUDSQL_USER');
+$password = getenv('CLOUDSQL_PASSWORD');
+
+// create the PDO client
 $db = new PDO($dsn, $user, $password);
 
 // create the tables if they don't exist
@@ -64,7 +60,6 @@ $results = $db->query('SELECT * from entries');
 // Now you can use the PDOStatement object to print or iterate over the results:
 //
 //     var_dump($results->fetchAll(PDO::FETCH_ASSOC));
-//
 # [END gae_cloudsql_example]
 ?>
 
