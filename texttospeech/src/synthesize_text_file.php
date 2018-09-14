7681c70d58db620aeba6e318dcb86bdf2c8c7dae
sirtorry <torryyang@google.com>
2018-09-14 14:15:10 -0700

    initial commit

---
+++ texttospeech/src/synthesize_text_file.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\TextToSpeech;
+
+// [START tts_synthesize_text_file]
+use Google\Cloud\V1\AudioConfig;
+use Google\Cloud\V1\AudioEncoding;
+use Google\Cloud\V1\SsmlVoiceGender;
+use Google\Cloud\V1\SynthesisInput;
+use Google\Cloud\V1\TextToSpeechClient;
+use Google\Cloud\V1\VoiceSelectionParams;
+
+function synthesize_text_file($path)
+{
+    // create client object
+    $client = new TextToSpeechClient();
+
+    // get text from file
+    $text = file_get_contents($path);
+    $input_text = (new SynthesisInput())
+        ->setText($text);
+
+    // note: the voice can also be specified by name
+    // names of voices can be retrieved with $client->listVoices()
+    $voice = (new VoiceSelectionParams())
+        ->setLanguageCode('en-US')
+        ->setSsmlGender(SsmlVoiceGender::FEMALE);
+
+    $audioConfig = (new AudioConfig())
+        ->setAudioEncoding(AudioEncoding::MP3);
+
+    $response = $client->synthesizeSpeech($input_text, $voice, $audioConfig);
+    $audioContent = $response->getAudioContent();
+
+    file_put_contents('output.mp3', $audioContent);
+    print('Audio content written to "output.mp3"' . PHP_EOL);
+
+    $client->close();
+}
+// [END tts_synthesize_text_file]
