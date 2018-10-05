6bc5aceaddf16a3c7662b91373162d6ca520cb47
Chris Deaton <flashadvocate@gmail.com>
2018-10-05 09:25:41 -0400

    brace variable
    This may be a matter of preference, and I hate to utter the words "good practice", but this may fall into a grey area. :)

--- appengine/php72/slim-framework/index.php
+++ appengine/php72/slim-framework/index.php
@@ -28,7 +28,7 @@ $app->get('/', function ($request, $response) {
     // Use the Null Coalesce Operator in PHP7
     // http://php.net/manual/en/language.operators.comparison.php#language.operators.comparison.coalesce
     $name = $request->getQueryParams()['name'] ?? 'World';
-    return $response->getBody()->write("Hello, $name!");
+    return $response->getBody()->write("Hello, {$name}!");
 });
 $app->run();
 # [END gae_slim_front_controller]
