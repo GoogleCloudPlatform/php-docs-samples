6b49ed75dab48ded51332fafe5cb57aeb5012f2a
Juan Rael <juan@qlogic.io>
2018-11-21 10:33:18 -0500

    Delete Instance and Delete Table

---
+++ bigtable/api/test/bigTableDeleteInstanceTest.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\BigTable\Tests;
+
+use Google\ApiCore\ApiException;
+use PHPUnit\Framework\TestCase;
+use Google\Cloud\Bigtable\Admin\V2\BigtableInstanceAdminClient;
+
+final class BigTableDeleteInstanceTest extends TestCase
+{
+	public function testDeleteInstance(): void
+    {
+        $project_id = getenv('PROJECT_ID');
+        $instance_id = 'php-delete-instance';
+        $cluster_id = 'php-delete-instance';
+
+        $instanceAdminClient = new BigtableInstanceAdminClient();
+        $instanceName = $instanceAdminClient->instanceName($project_id, $instance_id);
+
+        $this->runSnippet('create_production_instance', [
+            $project_id,
+            $instance_id,
+            $cluster_id
+        ]);
+        
+        try{
+            $instance = $instanceAdminClient->GetInstance($instanceName);
+            $this->assertEquals($instance->getName(), 'projects/' . $project_id . '/instances/' . $instance_id );
+        } catch (ApiException $e) {
+            if ($e->getStatus() === 'NOT_FOUND') {
+                $error = json_decode($e->getMessage(),true);
+                $this->fail($error['message']);
+            }
+            throw $e;
+        }
+
+        $content = $this->runSnippet('delete_instance', [
+            $project_id,
+            $instance_id
+        ]);
+
+        try{
+            $instance = $instanceAdminClient->GetInstance($instanceName);
+            $this->fail(sprintf('Instance %s still exists', $instance->getName()));
+        } catch (ApiException $e) {
+            if ($e->getStatus() === 'NOT_FOUND') {
+                $this->assertTrue(true);
+            }
+        }
+
+        $this->clean_instance($project_id, $instance_id, $cluster_id);
+    }
+
+    private function createTable($project_id, $instance_id, $cluster_id, $table_id)
+    {
+        $this->runSnippet('create_production_instance', [
+            $project_id,
+            $instance_id,
+            $cluster_id
+        ]);
+        $this->runSnippet('create_table', [
+            $project_id,
+            $instance_id,
+            $table_id
+        ]);
+    }
+	private function clean_instance($project_id, $instance_id, $cluster_id)
+    {
+        $content = $this->runSnippet('delete_instance', [
+            $project_id,
+            $instance_id
+        ]);
+    }
+
+    private function runSnippet($sampleName, $params = [])
+    {
+        $argv = array_merge([basename(__FILE__)], $params);
+        ob_start();
+        require_once __DIR__ . "/../src/$sampleName.php";
+        return ob_get_clean();
+    }
+
+}
---
+++ bigtable/api/test/bigTableDeleteTableTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\BigTable\Tests;
+
+use Google\Cloud\Bigtable\Admin\V2\BigtableTableAdminClient;
+use Google\Cloud\Bigtable\Admin\V2\Table\View;
+use Google\ApiCore\ApiException;
+use PHPUnit\Framework\TestCase;
+
+
+final class BigTableDeleteTableTest extends TestCase
+{
+	public function testDeleteTable(): void
+    {
+        $project_id = getenv('PROJECT_ID');
+        $instance_id = 'php-sample-instance-deltable';
+        $cluster_id = 'php-sample-cluster-deltable';
+        $table_id = 'php-sample-table-table';
+
+        $tableAdminClient = new BigtableTableAdminClient();
+        $tableName = $tableAdminClient->tableName($project_id, $instance_id, $table_id);
+
+        $this->runSnippet('create_production_instance', [
+            $project_id,
+            $instance_id,
+            $cluster_id
+        ]);
+        $this->runSnippet('create_table', [
+            $project_id,
+            $instance_id,
+            $table_id
+        ]);
+        
+        try{
+            $table = $tableAdminClient->getTable($tableName, ['view' => View::NAME_ONLY]);
+            $this->assertEquals($table->getName(), 'projects/' . $project_id . '/instances/' . $instance_id . '/tables/' . $table_id );
+        } catch (ApiException $e) {
+            if ($e->getStatus() === 'NOT_FOUND') {
+                $error = json_decode($e->getMessage(),true);
+                $this->fail($error['message']);
+            }
+            throw $e;
+        }
+
+        $content = $this->runSnippet('delete_instance', [
+            $project_id,
+            $instance_id
+        ]);
+
+        try{
+            $table = $tableAdminClient->getTable($tableName, ['view' => View::NAME_ONLY]);
+            $this->fail(sprintf('Instance %s still exists', $table->getName()));
+        } catch (ApiException $e) {
+            if ($e->getStatus() === 'NOT_FOUND') {
+                $this->assertTrue(true);
+            }
+        }
+
+        $this->clean_instance($project_id, $instance_id, $cluster_id);
+    }
+
+    private function createTable($project_id, $instance_id, $cluster_id, $table_id)
+    {
+        $this->runSnippet('create_production_instance', [
+            $project_id,
+            $instance_id,
+            $cluster_id
+        ]);
+        $this->runSnippet('create_table', [
+            $project_id,
+            $instance_id,
+            $table_id
+        ]);
+    }
+	private function clean_instance($project_id, $instance_id, $cluster_id)
+    {
+        $content = $this->runSnippet('delete_instance', [
+            $project_id,
+            $instance_id
+        ]);
+    }
+
+    private function runSnippet($sampleName, $params = [])
+    {
+        $argv = array_merge([basename(__FILE__)], $params);
+        ob_start();
+        require_once __DIR__ . "/../src/$sampleName.php";
+        return ob_get_clean();
+    }
+
+}
