d71cbe1b0ff8961283d70a82548e76858f4c3f02
Brent Shaffer <betterbrent@google.com>
2018-12-18 14:51:24 -0800

    uses curl instead of wget

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -44,12 +44,12 @@ mkdir -p build/logs
 
 export IS_PULL_REQUEST=$KOKORO_GITHUB_PULL_REQUEST_COMMIT
 
-# Run tests
-bash testing/run_test_suite.sh
-
-# Run code standards check on latest version of PHP
+# Run code standards check when appropriate
 if [ "${RUN_CS_CHECK}" = "true" ]; then
-  wget http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O php-cs-fixer \
+  curl -L https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -o php-cs-fixer \
        && chmod a+x php-cs-fixer
   bash testing/run_cs_check.sh
 fi
+
+# Run tests
+bash testing/run_test_suite.sh
