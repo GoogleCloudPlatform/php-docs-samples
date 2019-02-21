21f19cb28a5e5afacb6a70eecb5a0d43cec15b53
Brent Shaffer <betterbrent@google.com>
2019-02-21 14:36:21 -0800

    Update app.php

--- appengine/flexible/memcache/app.php
+++ appengine/flexible/memcache/app.php
@@ -25,20 +25,11 @@ $app = new Application();
 $app->register(new TwigServiceProvider());
 $app['twig.path'] = [ __DIR__ ];
 $app['memcached'] = function () {
-    if (getenv('USE_GAE_MEMCACHE')) {
-        $host = getenv('GAE_MEMCACHE_HOST') ?: 'localhost';
-        $port = getenv('GAE_MEMCACHE_PORT') ?: '11211';
-    } else {
-        $server = getenv('MEMCACHE_SERVER') ?: 'localhost:11211';
-        list($host, $port) = explode(':', $server);
-    }
-    $username = getenv('MEMCACHE_USERNAME');
-    $password = getenv('MEMCACHE_PASSWORD');
     # [START gae_flex_redislabs_memcache]
-    // $host = 'YOUR_MEMCACHE_HOST';
-    // $port = 'YOUR_MEMCACHE_PORT';
-    // $username = 'OPTIONAL_MEMCACHE_USERNAME';
-    // $password = 'OPTIONAL_MEMCACHE_PASSWORD';
+    $host = getenv('GAE_MEMCACHE_HOST') ?: 'localhost';
+    $port = getenv('GAE_MEMCACHE_PORT') ?: '11211';
+    $username = getenv('MEMCACHE_USERNAME'); // optional
+    $password = getenv('MEMCACHE_PASSWORD'); // optional
     $memcached = new Memcached;
     if ($username && $password) {
         $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
