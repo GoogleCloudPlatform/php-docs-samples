92e7f6bb6cbbb7cf77262931d89f0fd524e15f99
Brent Shaffer <betterbrent@google.com>
2019-07-03 09:36:15 -0600

    fixes quickstart test

--- spanner/test/quickstartTest.php
+++ spanner/test/quickstartTest.php
@@ -18,51 +18,34 @@
 namespace Google\Cloud\Samples\Spanner;
 
 use PHPUnit\Framework\TestCase;
+use Google\Cloud\TestUtils\TestTrait;
 
 class quickstartTest extends TestCase
 {
-    protected static $file;
+    use TestTrait;
 
     protected static $tempFile;
 
     public function setUp()
     {
-        if (!extension_loaded('grpc')) {
-            self::markTestSkipped('Must enable grpc extension.');
-        }
-        if (!$projectId = getenv('GOOGLE_PROJECT_ID')) {
-            $this->markTestSkipped('GOOGLE_PROJECT_ID must be set.');
-        }
-        if (!$instanceId = getenv('GOOGLE_SPANNER_INSTANCE_ID')) {
-            $this->markTestSkipped('GOOGLE_SPANNER_INSTANCE_ID must be set.');
-        }
-        if (!$databaseId = getenv('GOOGLE_SPANNER_DATABASE_ID')) {
-            $this->markTestSkipped('GOOGLE_SPANNER_DATABASE_ID must be set.');
-        }
+        $this->requireGrpc();
+        $instanceId = $this->requireEnv('GOOGLE_SPANNER_INSTANCE_ID');
+        $databaseId = $this->requireEnv('GOOGLE_SPANNER_DATABASE_ID');
 
         self::$tempFile = sys_get_temp_dir() . '/spanner_quickstart.php';
-        self::$file = __DIR__ . '/../quickstart.php';
-        copy(self::$file, self::$tempFile);
-        $contents = file_get_contents(__DIR__ . '/../quickstart.php');
         $contents = str_replace(
             ['YOUR_PROJECT_ID', 'your-instance-id', 'your-database-id', '__DIR__'],
-            [$projectId, $instanceId, $databaseId, sprintf('"%s/.."', __DIR__)],
-            $contents
+            [self::$projectId, $instanceId, $databaseId, sprintf('"%s/.."', __DIR__)],
+            file_get_contents(__DIR__ . '/../quickstart.php')
         );
-        file_put_contents(self::$file, $contents);
+        file_put_contents(self::$tempFile, $contents);
     }
 
     public function testQuickstart()
     {
         // Invoke quickstart.php
-        $results = include self::$file;
+        $output = $this->runSnippet(self::$tempFile);
 
-        $this->expectOutputString('Hello World' . PHP_EOL);
-    }
-
-    public function tearDown()
-    {
-        copy(self::$tempFile, self::$file);
-        unlink(self::$tempFile);
+        $this->assertContains('Hello World', $output);
     }
 }
