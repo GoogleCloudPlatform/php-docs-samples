19540564535c216e564a8fdde18d8d1c2ba253af
Brent Shaffer <betterbrent@google.com>
2019-07-10 12:25:42 -0600

    better error message for running global tests

--- testing/bootstrap.php
+++ testing/bootstrap.php
@@ -6,8 +6,13 @@ if (!file_exists($testDir . '/phpunit.xml.dist')) {
     throw new Exception('You are not in a test directory');
 }
 
+/**
+ * Our tests load global dependencies using coomposer
+ */
 if (!trait_exists(Google\Cloud\TestUtils\TestTrait::class)) {
-    throw new Exception('You need to run "bash testing/composer.sh" to installed the required global packages');
+    throw new Exception('You need to run "bash testing/composer.sh" to '
+        . 'installed the required global packages. Be sure to run these tests '
+        . 'using the global phpunit package (~/.composer/vendor/bin/phpunit)');
 }
 
 if (file_exists($testDir . '/composer.json')) {
