91906f57e017af420a331583788f9c8c6f252ee2
sirtorry <torryyang@google.com>
2019-12-05 10:45:42 -0800

    initial

---
+++ vision/src/detect_set_endpoint.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\Vision;
+
+use Google\Cloud\Vision\V1\Image;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+use Google\Cloud\Vision\V1\ImageSource;
+
+function detect_text($endpoint)
+{
+    // [START vision_set_endpoint]
+    # set endpoint
+    $options = ['api_endpoint' => $endpoint];
+
+    $client = new ImageAnnotatorClient($options);
+    // [END vision_set_endpoint]
+
+    # set image
+    $imageSource = (new ImageSource())
+        ->setImageUri('gs://cloud-samples-data/vision/text/screen.jpg');
+    $image = (new Image())
+        ->setSource($imageSource);
+
+    # annotate the image
+    $response = $client->textDetection($image);
+    $texts = $response->getTextAnnotations();
+
+    printf('%d texts found:' . PHP_EOL, count($texts));
+    foreach ($texts as $text) {
+        print($text->getDescription() . PHP_EOL);
+
+        # get bounds
+        $vertices = $text->getBoundingPoly()->getVertices();
+        $bounds = [];
+        foreach ($vertices as $vertex) {
+            $bounds[] = sprintf('(%d,%d)', $vertex->getX(), $vertex->getY());
+        }
+        print('Bounds: ' . join(', ', $bounds) . PHP_EOL);
+    }
+
+    $client->close();
+}
