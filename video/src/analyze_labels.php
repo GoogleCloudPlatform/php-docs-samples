cafe8579f34a9659efa64ce836c19093663741c9
sirtorry <torryyang@google.com>
2020-04-08 15:09:25 -0700

    switch region tags

--- video/src/analyze_labels.php
+++ video/src/analyze_labels.php
@@ -25,7 +25,7 @@ if (count($argv) < 2 || count($argv) > 3) {
 list($_, $uri) = $argv;
 $options = isset($argv[2]) ? ['pollingIntervalSeconds' => $argv[2]] : [];
 
-// [START video_analyze_labels]
+// [START video_analyze_labels_gcs]
 use Google\Cloud\VideoIntelligence\V1\VideoIntelligenceServiceClient;
 use Google\Cloud\VideoIntelligence\V1\Feature;
 
@@ -85,4 +85,4 @@ if ($operation->operationSucceeded()) {
 } else {
     print_r($operation->getError());
 }
-// [END video_analyze_labels]
+// [END video_analyze_labels_gcs]
