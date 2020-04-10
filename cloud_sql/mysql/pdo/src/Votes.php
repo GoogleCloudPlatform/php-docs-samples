ac7e74a37e6aabd2ae980060bd686aecf9b5384b
John Pedrie <john@pedrie.com>
2020-04-10 12:23:21 -0400

    feat: add mysql sample

---
+++ cloud_sql/mysql/pdo/src/Votes.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\CloudSQL\MySQL;
+
+use PDO;
+
+class Votes
+{
+    private $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+        $this->create_table();
+    }
+
+    private function create_table()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS votes (
+            vote_id INT NOT NULL AUTO_INCREMENT,
+            time_cast DATETIME NOT NULL,
+            candidate VARCHAR(6) NOT NULL,
+            PRIMARY KEY (vote_id)
+        );";
+
+        $this->connection->exec($sql);
+    }
+
+    public function list()
+    {
+        $sql = "SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function count_candidates()
+    {
+        $sql = "SELECT COUNT(vote_id) as voteCount FROM votes WHERE candidate = ?";
+        $count = [];
+
+        $statement = $this->connection->prepare($sql);
+
+        //tabs
+        $statement->execute(['TABS']);
+        $count['tabs'] = $statement->fetch()[0];
+
+        //spaces
+        $statement->execute(['SPACES']);
+        $count['spaces'] = $statement->fetch()[0];
+
+        return $count;
+    }
+
+    public function save($team)
+    {
+        $sql = "INSERT INTO votes (time_cast, candidate) VALUES (NOW(), :candidate)";
+        $statement = $this->connection->prepare($sql);
+        $statement->bindParam('candidate', $team);
+
+        if ($statement->execute()) {
+            return "Vote successfully cast for '$team'";
+        }
+
+        return print_r($statement->errorInfo(), true);
+    }
+}
