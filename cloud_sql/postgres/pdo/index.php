c31bd095c7cf752dec05e072ca5a404771644cfd
John Pedrie <john@pedrie.com>
2020-04-30 14:31:44 -0400

    feat: refactor postgres sample to use slim app

--- cloud_sql/postgres/pdo/index.php
+++ cloud_sql/postgres/pdo/index.php
@@ -1,40 +1,54 @@
 <?php
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-require_once 'src/DB.php';
-require_once 'src/Votes.php';
+declare(strict_types=1);
 
-use Google\Cloud\Samples\CloudSQL\Postgres\DB;
-use Google\Cloud\Samples\CloudSQL\Postgres\Votes;
+use GuzzleHttp\Psr7;
 
-$votes = new Votes(DB::createPdoConnection());
+include __DIR__ . '/vendor/autoload.php';
 
-if ($_SERVER['REQUEST_URI'] == '/' && $_SERVER['REQUEST_METHOD'] == 'GET') {
-    $list = $votes->list();
+$app = include __DIR__ . '/src/app.php';
 
-    $vote_count = $votes->count_candidates();
-    $tab_count = $vote_count['tabs'];
-    $space_count = $vote_count['spaces'];
+$app->get('/', function ($request, $response) {
+    $this->get('votes')->createTableIfNotExists();
 
-    include_once("./template.php");
-} elseif ($_SERVER['REQUEST_URI'] == '/' && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    return $this->get('view')->render($response, 'template.twig', [
+        'votes' => $this->get('votes')->listVotes(),
+        'tabCount' => $this->get('votes')->getCountByValue('TABS'),
+        'spaceCount' => $this->get('votes')->getCountByValue('SPACES'),
+    ]);
+});
+
+$app->post('/', function ($request, $response) {
+    $this->get('votes')->createTableIfNotExists();
+
     $message = 'Invalid vote. Choose Between TABS and SPACES';
 
-    if (!empty($_POST['team']) && in_array($_POST['team'], ['SPACES', 'TABS'])) {
-        $message = $votes->save($_POST['team']);
+    $formData = $request->getParsedBody() + [
+        'voteValue' => ''
+    ];
+
+    if (in_array($formData['voteValue'], ['SPACES', 'TABS'])) {
+        $message = $this->get('votes')->insertVote($formData['voteValue'])
+            ? 'Vote cast for ' . $formData['voteValue']
+            : 'An error occurred';
     }
 
-    echo $message;
-}
+    return $response->withBody(Psr7\stream_for($message));
+});
+
+$app->run();
--- cloud_sql/postgres/pdo/src/Votes.php
+++ cloud_sql/postgres/pdo/src/Votes.php
@@ -1,81 +1,127 @@
 <?php
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
 
 namespace Google\Cloud\Samples\CloudSQL\Postgres;
 
 use PDO;
+use PDOException;
+use RuntimeException;
 
+/**
+ * Manage votes using the Cloud SQL database.
+ */
 class Votes
 {
+    /**
+     * @var PDO
+     */
     private $connection;
 
+    /**
+     * @param PDO $connection A connection to the database.
+     */
     public function __construct(PDO $connection)
     {
         $this->connection = $connection;
-        $this->create_table();
     }
 
-    private function create_table()
+    /**
+     * Creates the table if it does not yet exist.
+     *
+     * @return void
+     */
+    public function createTableIfNotExists()
     {
-        $sql = "
-        CREATE TABLE IF NOT EXISTS votes (
-            vote_id SERIAL NOT NULL,
-            time_cast TIMESTAMP NOT NULL,
-            candidate VARCHAR(6) NOT NULL,
-            PRIMARY KEY (vote_id)
-        );";
-        $this->connection->exec($sql);
+        try {
+            $stmt = $this->connection->prepare('SELECT 1 FROM votes');
+            $stmt->execute();
+        } catch (PDOException $e) {
+            $sql = "CREATE TABLE votes (
+                vote_id SERIAL NOT NULL,
+                time_cast TIMESTAMP NOT NULL,
+                vote_value VARCHAR(6) NOT NULL,
+                PRIMARY KEY (vote_id)
+            );";
+
+            $this->connection->exec($sql);
+        }
     }
 
-    public function list()
+    /**
+     * Returns a list of the last five votes
+     *
+     * @return array
+     */
+    public function listVotes() : array
     {
-        $sql = "SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5";
+        $sql = "SELECT vote_value, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5";
         $statement = $this->connection->prepare($sql);
         $statement->execute();
-
-        return $statement->fetchAll();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function count_candidates()
+    /**
+     * Get the number of votes cast for a given value.
+     *
+     * @param string $value
+     * @param int
+     */
+    public function getCountByValue(string $value) : int
     {
-        $sql = "SELECT COUNT(vote_id) FROM votes WHERE candidate = ?";
-        $count = [];
+        $sql = "SELECT COUNT(vote_id) as voteCount FROM votes WHERE vote_value = ?";
 
         $statement = $this->connection->prepare($sql);
+        $statement->execute([$value]);
 
-        //tabs
-        $statement->execute(['TABS']);
-        $count['tabs'] = $statement->fetch()['count'];
-
-        //spaces
-        $statement->execute(['SPACES']);
-        $count['spaces'] = $statement->fetch()['count'];
-
-        return $count;
+        return (int) $statement->fetch(PDO::FETCH_COLUMN);
     }
 
-    public function save($team)
+    /**
+     * Insert a new vote into the database
+     *
+     * @param string $value The value to vote for.
+     * @return boolean
+     */
+    public function insertVote(string $value) : bool
     {
-        $sql = "INSERT INTO votes (time_cast, candidate) VALUES ('NOW()', :candidate)";
-        $statement = $this->connection->prepare($sql);
-        $statement->bindParam('candidate', $team);
+        $conn = $this->connection;
+        $res = false;
 
-        if ($statement->execute()) {
-            return "Vote successfully cast for '$team'";
+        # [START cloud_sql_postgres_pdo_connection]
+        // Use prepared statements to guard against SQL injection.
+        $sql = "INSERT INTO votes (time_cast, vote_value) VALUES (NOW(), :voteValue)";
+
+        try {
+            $statement = $conn->prepare($sql);
+            $statement->bindParam('voteValue', $value);
+
+            $res = $statement->execute();
+        } catch (PDOException $e) {
+            throw new RuntimeException(
+                "Could not insert vote into database. The PDO exception was " .
+                $e->getMessage(),
+                $e->getCode(),
+                $e
+            );
         }
+        # [END cloud_sql_postgres_pdo_connection]
 
-        return $this->connection->errorInfo();
+        return $res;
     }
 }
