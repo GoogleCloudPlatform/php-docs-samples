bf76f2016070ecb9ae3e512e6ba1147600ae2d14
MartinBarriga <martin.barriga.vargas@gmail.com>
2020-06-12 02:04:59 +0000

    update licenses and apply suggested format changes

--- tasks/test/tasksTest.php
+++ tasks/test/tasksTest.php
@@ -1,19 +1,17 @@
 <?php
-/**
- * Copyright 2019 Google LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Google\Cloud\Samples\Tasks\Tests;
 
@@ -33,7 +31,7 @@ class TasksTest extends TestCase
     private static $email;
     private static $payload;
     private static $haveVariablesBeenInitialized = false;
-
+    
     public function testCreateHttpTask()
     {
         if(!self::$haveVariablesBeenInitialized){
@@ -48,7 +46,8 @@ class TasksTest extends TestCase
             self::$payload,
         ]);
 
-        $expectedOutput = $this->getExpectedOutput();
+        $taskNamePrefix = $this->getTaskNamePrefix();
+        $expectedOutput = sprintf('Created task %s', $taskNamePrefix);
         $this->assertContains($expectedOutput, $output);
     }
 
@@ -67,7 +66,8 @@ class TasksTest extends TestCase
             self::$payload,
         ]);
 
-        $expectedOutput = $this->getExpectedOutput();
+        $taskNamePrefix = $this->getTaskNamePrefix();
+        $expectedOutput = sprintf('Created task %s', $taskNamePrefix);
         $this->assertContains($expectedOutput, $output);
     }
 
@@ -79,15 +79,14 @@ class TasksTest extends TestCase
         self::$payload = 'Task Details';
     }
 
-    private function getExpectedOutput()
+    private function getTaskNamePrefix()
     {
         $taskNamePrefix = sprintf('projects/%s/locations/%s/queues/%s/tasks/',
             self::$projectId,
             self::$location,
             self::$queue
         );
-        $expectedOutput = sprintf('Created task %s', $taskNamePrefix);
-        return $expectedOutput;
+        return $taskNamePrefix;
     }
 
     private function runSnippet($sampleName, $params = [])
@@ -98,4 +97,5 @@ class TasksTest extends TestCase
         require __DIR__ . "/../src/$sampleName.php";
         return ob_get_clean();
     }
+    
 }
