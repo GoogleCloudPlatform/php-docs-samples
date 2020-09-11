35a7fa0a0f621a841d937b0274db5bfa04aa5d3a
Adam Ross <adamross@google.com>
2020-09-11 15:15:36 -0700

    run: fix missing file terminating newline

--- run/helloworld-php/index.php
+++ run/helloworld-php/index.php
@@ -1,7 +1,8 @@
 <?php
+
 // [START run_helloworld_service]
 
 $name = getenv('NAME', true) ?: 'World';
-echo sprintf("Hello %s!", $name);
+echo sprintf('Hello %s!', $name);
 
 // [END run_helloworld_service]
