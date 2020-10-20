bbbe187279f295ba76174113c27938dbddbf39ee
ace-n <anassri@google.com>
2020-10-19 20:55:13 -0700

    add functions_concepts_filesystem

---
+++ functions/concepts_filesystem/test/SystemTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\Functions\HelloworldGet\Test;
+
+use PHPUnit\Framework\TestCase;
+use Google\Cloud\TestUtils\CloudFunctionLocalTestTrait;
+
+/**
+ * Class SystemTest.
+ */
+class SystemTest extends TestCase
+{
+    use CloudFunctionLocalTestTrait;
+
+    private static $name = 'listFiles';
+
+    public function testFunction() : void
+    {
+        // Send a request to the function.
+        $resp = $this->client->get('/');
+
+        // Assert status code.
+        $this->assertEquals('200', $resp->getStatusCode());
+
+        // Assert function output.
+        $expected = trim('index.php');
+        $actual = trim((string) $resp->getBody());
+        $this->assertContains($expected, $actual);
+    }
+}
---
+++ functions/concepts_filesystem/test/UnitTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\Functions\HelloworldGet\Test;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for the Cloud Function.
+ */
+class UnitTest extends TestCase
+{
+    private static $name = 'listFiles';
+
+    public static function setUpBeforeClass() : void
+    {
+        require_once __DIR__ . '/../index.php';
+    }
+
+    public function testFunction() : void
+    {
+        $request = new ServerRequest('GET', '/');
+        $this->runFunction(self::$name, [$request]);
+        $this->expectOutputRegex("/index\.php/");
+    }
+
+    private static function runFunction($functionName, array $params = []) : string
+    {
+        return call_user_func_array($functionName, $params);
+    }
+}
