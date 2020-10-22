a53ec7868eab887d74cf80377ebf847d2d09f0d4
Dan O'Meara <danom@google.com>
2020-10-21 22:05:46 -0700

    chore: updates region tag (#1200)

--- video/src/analyze_text_detection.php
+++ video/src/analyze_text_detection.php
@@ -25,7 +25,7 @@ if (count($argv) < 2 || count($argv) > 3) {
 list($_, $uri) = $argv;
 $options = isset($argv[2]) ? ['pollingIntervalSeconds' => $argv[2]] : [];
 
-// [START video_analyze_text_detection]
+// [START video_detect_text_gcs]
 use Google\Cloud\VideoIntelligence\V1\VideoIntelligenceServiceClient;
 use Google\Cloud\VideoIntelligence\V1\Feature;
 
@@ -65,4 +65,4 @@ if ($operation->operationSucceeded()) {
 } else {
     print_r($operation->getError());
 }
-// [END video_analyze_text_detection]
+// [END video_detect_text_gcs]
