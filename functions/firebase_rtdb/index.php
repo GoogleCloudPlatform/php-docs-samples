d3a5c2453712913c8b8e995ff4d975f1a37947f9
ace-n <anassri@google.com>
2020-11-15 00:45:54 -0800

    [WIP] feat(functions): add Firebase RTDB sample

---
+++ functions/firebase_rtdb/index.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START functions_firebase_rtdb]
+
+use Google\CloudFunctions\CloudEvent;
+
+function firebaseRTDB(CloudEvent $cloudevent)
+{
+    $log = fopen(getenv('LOGGER_OUTPUT') ?: 'php://stderr', 'wb');
+    
+    fwrite($log, "Event: " . $cloudevent->getId() . PHP_EOL);
+
+    $data = $cloudevent->getData();
+    $resource = $data['resource'];
+
+    if (isset($data['params'])) {
+        fwrite($log, 'Path parameters:');
+        foreach ($data['params'] as $param => $value) {
+            fwrite($log, '  ' . $param . ': ' . $value . PHP_EOL);
+        }
+    }
+
+    fwrite($log, 'Function triggered by change to: ' . $resource . PHP_EOL);
+
+    $isAdmin =
+        isset($data['auth']) &&
+        isset($data['auth']['admin']) &&
+        $data['auth']['admin'] == true;
+
+    fwrite($log, 'Admin?: ' . $isAdmin . PHP_EOL);
+    fwrite($log, 'Delta: ' . json_encode($data['delta']) . PHP_EOL);
+}
+// [END functions_firebase_rtdb]
---
+++ functions/firebase_rtdb/test/DeployTest.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\Functions\HelloworldStorage\Test;
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Database;
+
+use Google\Cloud\Logging\LoggingClient;
+use Google\Cloud\TestUtils\CloudFunctionDeploymentTrait;
+use Google\Cloud\TestUtils\EventuallyConsistentTestTrait;
+use Google\Cloud\TestUtils\GcloudWrapper\CloudFunction;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\ExpectationFailedException;
+
+/**
+ * Class DeployTest.
+ *
+ * This test is not run by the CI system.
+ *
+ * To skip deployment of a new function, run with "GOOGLE_SKIP_DEPLOYMENT=true".
+ * To skip deletion of the tested function, run with "GOOGLE_KEEP_DEPLOYMENT=true".
+ */
+class DeployTest extends TestCase
+{
+    use CloudFunctionDeploymentTrait;
+    use EventuallyConsistentTestTrait;
+
+    /** @var string */
+    private static $entryPoint = 'firebaseRTDB';
+    
+    /** @var string */
+    private static $functionSignatureType = 'cloudevent';
+
+    /** @var string */
+    private static $rtdbPath = 'foods';
+
+    /** @var LoggingClient */
+    private static $loggingClient;
+
+    /** @var Database */
+    private static $database;
+
+    /**
+     * Deploy the Cloud Function, called from DeploymentTrait::deployApp().
+     *
+     * Overrides CloudFunctionDeploymentTrait::doDeploy().
+     */
+    private static function doDeploy()
+    {
+        $project = self::requireEnv('GOOGLE_PROJECT_ID');
+
+        $resource =
+            'projects/_/instances/' . $project . '/refs/' . self::$rtdbPath;
+        $event = 'providers/google.firebase.database/eventTypes/ref.write';
+
+        return self::$fn->deploy([
+            '--trigger-resource' => $resource,
+            '--trigger-event' => $event
+        ], '');
+    }
+
+    public function dataProvider()
+    {
+        $data = array('taco' => (string) uniqid());
+        return [
+            [
+                'data' => $data,
+                'expected' => json_encode($data)
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFirebaseRTDB(array $data, string $expected): void
+    {
+        // Trigger storage upload.
+        $objectUri = $this->updateRTDB(self::$rtdbPath, $data);
+
+        // Give event and log systems a head start.
+        // If log retrieval fails to find logs for our function within retry limit, increase sleep time.
+        sleep(5);
+
+        $fiveMinAgo = date(\DateTime::RFC3339, strtotime('-5 minutes'));
+        $this->processFunctionLogs(self::$fn, $fiveMinAgo, function (\Iterator $logs) use ($expected) {
+            // Concatenate all relevant log messages.
+            $actual = '';
+            foreach ($logs as $log) {
+                $info = $log->info();
+                if (isset($info['textPayload'])) {
+                    $actual .= $info['textPayload'];
+                }
+            }
+
+            // Only testing one property to decrease odds the expected logs are
+            // split between log requests.
+            $this->assertContains($expected, $actual);
+        });
+    }
+
+    /**
+     * Retrieve and process logs for the defined function.
+     *
+     * @param CloudFunction $fn function whose logs should be checked.
+     * @param string $startTime RFC3339 timestamp marking start of time range to retrieve.
+     * @param callable $process callback function to run on the logs.
+     */
+    private function processFunctionLogs(CloudFunction $fn, string $startTime, callable $process)
+    {
+        $projectId = self::requireEnv('GOOGLE_PROJECT_ID');
+
+        if (empty(self::$loggingClient)) {
+            self::$loggingClient = new LoggingClient([
+                'projectId' => $projectId
+            ]);
+        }
+
+        // Define the log search criteria.
+        $logFullName = 'projects/' . $projectId . '/logs/cloudfunctions.googleapis.com%2Fcloud-functions';
+        $filter = sprintf(
+            'logName="%s" resource.labels.function_name="%s" timestamp>="%s"',
+            $logFullName,
+            $fn->getFunctionName(),
+            $startTime
+        );
+
+        echo "\nRetrieving logs [$filter]...\n";
+
+        // Check for new logs for the function.
+        $attempt = 1;
+        $this->runEventuallyConsistentTest(function () use ($filter, $process, &$attempt) {
+            $entries = self::$loggingClient->entries(['filter' => $filter]);
+ 
+            // If no logs came in try again.
+            if (empty($entries->current())) {
+                echo 'Logs not found, attempting retry #' . $attempt++ . PHP_EOL;
+                throw new ExpectationFailedException('Log Entries not available');
+            }
+            echo 'Processing logs...' . PHP_EOL;
+
+            $process($entries);
+        }, $retries = 10);
+    }
+
+    /**
+     * Update a value in Firebase Realtime Database (RTDB).
+     *
+     * @param string $path Path of the RTDB attribute to set.
+     * @param string $value The value to set the specified attribute to.
+     * @param string $data Data to upload as an object..
+     *
+     * @throws \RuntimeException
+     */
+    private function updateRTDB(string $path, array $data): void
+    {
+        if (empty(self::$storageClient)) {
+            $factory = new Factory();
+            self::$database = $factory->createDatabase();
+        }
+
+        self::$database->getReference($path)->set($data);
+    }
+}
