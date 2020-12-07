8d453dfcb0909228ae720adc0f2be7036bf26f1a
b-loved-dreamer <franklinsnunez@gmail.com>
2020-12-07 15:05:24 -0800

    fix: uncommented important line

--- speech/test/speechTest.php
+++ speech/test/speechTest.php
@@ -22,7 +22,7 @@ use PHPUnit\Framework\TestCase;
 
 class speechTest extends TestCase
 {
-    // use TestTrait;
+    use TestTrait;
 
     protected static $bucketName;
 
@@ -92,8 +92,8 @@ class speechTest extends TestCase
             ['transcribe_async', __DIR__ . '/data/audio32KHz.raw'],
             ['transcribe_async_gcs', 'gs://' . self::$bucketName . '/speech/audio32KHz.raw'],
             ['transcribe_async_words', __DIR__ . '/data/audio32KHz.raw'],
-            ['profanity_filter', __DIR__ . '/data/audio32KHz.raw'],
             ['profanity_filter_gcs', 'gs://' . self::$bucketName . '/speech/audio32KHz.raw'],
+            ['profanity_filter', __DIR__ . '/data/audio32KHz.raw'],
             ['streaming_recognize', __DIR__ . '/data/audio32KHz.raw', true],
         ];
     }
