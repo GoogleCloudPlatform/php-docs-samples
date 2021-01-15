78e11337508ad8c65ea3391aec586aa92b284be6
Anthony Wens <awenz@google.com>
2021-01-15 23:58:30 +0000

    docs: lint

--- securitycenter/src/create_notification.php
+++ securitycenter/src/create_notification.php
@@ -22,7 +22,7 @@ if (count($argv) < 4) {
 }
 list($_, $organizationId, $notificationConfigId, $projectId, $topicName) = $argv;
 
-// [START securitycenter_ create_notification_config]
+// [START securitycenter_create_notification_config]
 // [START scc_create_notification_config]
 use Google\Cloud\SecurityCenter\V1\SecurityCenterClient;
 use Google\Cloud\SecurityCenter\V1\NotificationConfig;
@@ -52,4 +52,4 @@ $response = $securityCenterClient->createNotificationConfig(
 printf('Notification config was created: %s' . PHP_EOL, $response->getName());
 
 // [END scc_create_notification_config]
-// [END securitycenter_ create_notification_config]
+// [END securitycenter_create_notification_config]
