906d56f233954dfcaa47237d491851234608257c
Craig Labenz <craig.labenz@gmail.com>
2021-01-25 14:08:08 -0800

    Added integration tests for Cloud SQL (#1251)

--- cloud_sql/postgres/pdo/src/app.php
+++ cloud_sql/postgres/pdo/src/app.php
@@ -17,6 +17,7 @@
 
 declare(strict_types=1);
 
+use Google\Cloud\Samples\CloudSQL\Postgres\DBInitializer;
 use Google\Cloud\Samples\CloudSQL\Postgres\Votes;
 use Pimple\Container;
 use Pimple\Psr11\Container as Psr11Container;
@@ -38,131 +39,48 @@ $container['db'] = function () {
     # [START cloud_sql_postgres_pdo_timeout]
     // Here we set the connection timeout to five seconds and ask PDO to
     // throw an exception if any errors occur.
-    $conn_config = [
+    $connConfig = [
         PDO::ATTR_TIMEOUT => 5,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ];
     # [END cloud_sql_postgres_pdo_timeout]
 
-    if (empty(getenv('DB_HOST'))) {
-        return init_unix_database_connection($conn_config);
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $dbName = getenv('DB_NAME');
+
+    if (empty($username = getenv('DB_USER'))) {
+        throw new RuntimeException('Must supply $DB_USER environment variables');
+    }
+    if (empty($password = getenv('DB_PASS'))) {
+        throw new RuntimeException('Must supply $DB_PASS environment variables');
+    }
+    if (empty($dbName = getenv('DB_NAME'))) {
+        throw new RuntimeException('Must supply $DB_NAME environment variables');
+    }
+
+    if ($dbHost = getenv('DB_HOST')) {
+        return DBInitializer::initTcpDatabaseConnection(
+            $username,
+            $password,
+            $dbName,
+            $dbHost,
+            $connConfig
+        );
     } else {
-        return init_tcp_database_connection($conn_config);
+        $connectionName = getenv('CLOUDSQL_CONNECTION_NAME');
+        $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
+        return DBInitializer::initUnixDatabaseConnection(
+            $username,
+            $password,
+            $dbName,
+            $connectionName,
+            $socketDir,
+            $connConfig
+        );
     }
 };
 
-/**
- *  @param $conn_config array driver-specific options for PDO
- */
-function init_tcp_database_connection(array $conn_config): PDO
-{
-    $username = getenv('DB_USER');
-    $password = getenv('DB_PASS');
-    $db_name = getenv('DB_NAME');
-    $host = getenv('DB_HOST');
-
-    try {
-        # [START cloud_sql_postgres_pdo_create_tcp]
-        // $username = 'your_db_user';
-        // $password = 'yoursupersecretpassword';
-        // $db_name = 'your_db_name';
-        // $host = "127.0.0.1";
-
-        // Connect using TCP
-        $dsn = sprintf('pgsql:dbname=%s;host=%s', $db_name, $host);
-
-        // Connect to the database
-        $conn = new PDO($dsn, $username, $password, $conn_config);
-        # [END cloud_sql_postgres_pdo_create_tcp]
-    } catch (TypeError $e) {
-        throw new RuntimeException(
-            sprintf(
-                'Invalid or missing configuration! Make sure you have set ' .
-                '$username, $password, $db_name, and $host (for TCP mode) ' .
-                'or $cloud_sql_connection_name (for UNIX socket mode). ' .
-                'The PHP error was %s',
-                $e->getMessage()
-            ),
-            $e->getCode(),
-            $e
-        );
-    } catch (PDOException $e) {
-        throw new RuntimeException(
-            sprintf(
-                'Could not connect to the Cloud SQL Database. Check that ' .
-                'your username and password are correct, that the Cloud SQL ' .
-                'proxy is running, and that the database exists and is ready ' .
-                'for use. For more assistance, refer to %s. The PDO error was %s',
-                'https://cloud.google.com/sql/docs/postgres/connect-external-app',
-                $e->getMessage()
-            ),
-            $e->getCode(),
-            $e
-        );
-    }
-
-    return $conn;
-}
-
-/**
- *  @param $conn_config array driver-specific options for PDO
- */
-function init_unix_database_connection(array $conn_config)
-{
-    $username = getenv('DB_USER');
-    $password = getenv('DB_PASS');
-    $db_name = getenv('DB_NAME');
-    $cloud_sql_connection_name = getenv('CLOUD_SQL_CONNECTION_NAME');
-    $socket_dir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
-
-    try {
-        # [START cloud_sql_postgres_pdo_create_socket]
-        // $username = 'your_db_user';
-        // $password = 'yoursupersecretpassword';
-        // $db_name = 'your_db_name';
-        // $cloud_sql_connection_name = getenv("CLOUD_SQL_CONNECTION_NAME");
-        // $socket_dir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
-
-        // Connect using UNIX sockets
-        $dsn = sprintf(
-            'pgsql:dbname=%s;host=%s/%s',
-            $db_name,
-            $socket_dir,
-            $cloud_sql_connection_name
-        );
-
-        // Connect to the database.
-        $conn = new PDO($dsn, $username, $password, $conn_config);
-        # [END cloud_sql_postgres_pdo_create_socket]
-    } catch (TypeError $e) {
-        throw new RuntimeException(
-            sprintf(
-                'Invalid or missing configuration! Make sure you have set ' .
-                '$username, $password, $db_name, and $host (for TCP mode) ' .
-                'or $cloud_sql_connection_name (for UNIX socket mode). ' .
-                'The PHP error was %s',
-                $e->getMessage()
-            ),
-            $e->getCode(),
-            $e
-        );
-    } catch (PDOException $e) {
-        throw new RuntimeException(
-            sprintf(
-                'Could not connect to the Cloud SQL Database. Check that ' .
-                'your username and password are correct, that the Cloud SQL ' .
-                'proxy is running, and that the database exists and is ready ' .
-                'for use. For more assistance, refer to %s. The PDO error was %s',
-                'https://cloud.google.com/sql/docs/postgres/connect-external-app',
-                $e->getMessage()
-            ),
-            $e->getCode(),
-            $e
-        );
-    }
-
-    return $conn;
-}
 
 // Configure the templating engine.
 $container['view'] = function () {
