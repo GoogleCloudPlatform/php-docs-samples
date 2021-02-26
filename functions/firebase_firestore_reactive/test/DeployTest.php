714f97938c76cdbeaab4fa238cc3cb3112d0d630
ace-n <anassri@google.com>
2021-02-25 18:40:35 -0800

    feat(functions): add Firestore reactive sample

---
+++ functions/firebase_firestore_reactive/test/DeployTest.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\Functions\FirebaseReactive\Test;
+
+use Google\Cloud\Firestore\FirestoreClient;
+use Google\Cloud\Logging\LoggingClient;
+use Google\Cloud\TestUtils\CloudFunctionDeploymentTrait;
+use Google\Cloud\TestUtils\EventuallyConsistentTestTrait;
+use Google\Cloud\TestUtils\GcloudWrapper\CloudFunction;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\ExpectationFailedException;
+
+/**
+ * Class DeployTest.
+ *
+ * This test is not run by the CI system.
+ *
+ * To skip deployment of a new function, run with "GOOGLE_SKIP_DEPLOYMENT=true".
+ * To skip deletion of the tested function, run with "GOOGLE_KEEP_DEPLOYMENT=true".
+ */
+class DeployTest extends TestCase
+{
+    use CloudFunctionDeploymentTrait;
+    use EventuallyConsistentTestTrait;
+
+    /** @var string */
+    private static $entryPoint = 'firebaseReactive';
+
+    /** @var string */
+    private static $functionSignatureType = 'cloudevent';
+
+    /** @var string */
+    private static $collectionName = 'messages';
+
+    /** @var string */
+    private static $documentName = 'taco';
+
+    /** @var LoggingClient */
+    private static $loggingClient;
+
+    /** @var FirestoreClient */
+    private static $firestoreClient;
+
+    /**
+     * Deploy the Cloud Function, called from DeploymentTrait::deployApp().
+     *
+     * Overrides CloudFunctionDeploymentTrait::doDeploy().
+     */
+    private static function doDeploy()
+    {
+        $project = self::requireEnv('GOOGLE_PROJECT_ID');
+
+        $resource =
+            'projects/' . $project . '/databases/(default)/documents/' . self::$collectionName . '/' . self::$documentName;
+        $event = 'providers/cloud.firestore/eventTypes/document.write';
+
+        $projectId = self::requireEnv('GOOGLE_PROJECT_ID');
+        $envVar = 'GOOGLE_PROJECT_ID=' . $projectId;
+
+        return self::$fn->deploy([
+            '--update-env-vars' => $envVar,
+            '--trigger-resource' => $resource,
+            '--trigger-event' => $event
+        ], '');
+    }
+
+    public function dataProvider()
+    {
+        $data = uniqid();
+        $expected = 'Replacing value: ' . $data . ' --> ' . strtoupper($data);
+        return [
+            [
+                'data' => ['original' => $data],
+                'expected' => $expected
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFirebaseReactive(array $data, string $expected): void
+    {
+        // Trigger storage upload.
+        $objectUri = $this->updateFirestore(
+            self::$collectionName,
+            self::$documentName,
+            $data
+        );
+
+        // Give event and log systems a head start.
+        // If log retrieval fails to find logs for our function within retry limit, increase sleep time.
+        sleep(30);
+
+        $fiveMinAgo = date(\DateTime::RFC3339, strtotime('-5 minutes'));
+        $this->processFunctionLogs(self::$fn, $fiveMinAgo, function (\Iterator $logs) use ($expected) {
+            // Concatenate all relevant log messages.
+            $actual = '';
+            foreach ($logs as $log) {
+                $info = $log->info();
+                if (isset($info['textPayload'])) {
+                    $actual .= $info['textPayload'];
+                }
+            }
+
+            // Only testing one property to decrease odds the expected logs are
+            // split between log requests.
+            var_dump($expected);
+            var_dump($actual);
+            $this->assertStringContainsString($expected, $actual);
+        });
+    }
+
+    /**
+     * Retrieve and process logs for the defined function.
+     *
+     * @param CloudFunction $fn function whose logs should be checked.
+     * @param string $startTime RFC3339 timestamp marking start of time range to retrieve.
+     * @param callable $process callback function to run on the logs.
+     */
+    private function processFunctionLogs(CloudFunction $fn, string $startTime, callable $process)
+    {
+        $projectId = self::requireEnv('GOOGLE_PROJECT_ID');
+
+        if (empty(self::$loggingClient)) {
+            self::$loggingClient = new LoggingClient([
+                'projectId' => $projectId
+            ]);
+        }
+
+        // Define the log search criteria.
+        $logFullName = 'projects/' . $projectId . '/logs/cloudfunctions.googleapis.com%2Fcloud-functions';
+        $filter = sprintf(
+            'logName="%s" resource.labels.function_name="%s" timestamp>="%s"',
+            $logFullName,
+            $fn->getFunctionName(),
+            $startTime
+        );
+
+        echo "\nRetrieving logs [$filter]...\n";
+
+        // Check for new logs for the function.
+        $attempt = 1;
+        $this->runEventuallyConsistentTest(function () use ($filter, $process, &$attempt) {
+            $entries = self::$loggingClient->entries(['filter' => $filter]);
+
+            // If no logs came in try again.
+            if (empty($entries->current())) {
+                echo 'Logs not found, attempting retry #' . $attempt++ . PHP_EOL;
+                throw new ExpectationFailedException('Log Entries not available');
+            }
+            echo 'Processing logs...' . PHP_EOL;
+
+            $process($entries);
+        }, $retries = 10);
+    }
+
+    /**
+     * Update a value in Firebase Realtime Database (RTDB).
+     *
+     * @param string $document The Firestore document to modify.
+     * @param string $collection The Firestore collection to modify.
+     * @param string $data The key-value pair to set the specified collection to.
+     *
+     * @throws \RuntimeException
+     */
+    private function updateFirestore(
+        string $document,
+        string $collection,
+        array $data
+    ): void {
+        if (empty(self::$firestore)) {
+            self::$firestoreClient = new FirestoreClient();
+        }
+
+        self::$firestoreClient
+            ->collection(self::$collectionName)
+            ->document(self::$documentName)
+            ->set($data);
+    }
+}
