7ff5022ebba99b132113aa24a50fd85bb79e77a7
Brent Shaffer <betterbrent@google.com>
2021-03-23 12:34:16 -0700

    fix(asset): sample warnings, upgrade to new snippet format (#1311)

--- asset/test/assetTest.php
+++ asset/test/assetTest.php
@@ -19,7 +19,6 @@ namespace Google\Cloud\Samples\Asset;
 
 use Google\Cloud\Storage\StorageClient;
 use Google\Cloud\TestUtils\TestTrait;
-use Google\Cloud\TestUtils\ExecuteCommandTrait;
 use Google\Cloud\TestUtils\EventuallyConsistentTestTrait;
 use PHPUnit\Framework\TestCase;
 
@@ -29,10 +28,8 @@ use PHPUnit\Framework\TestCase;
 class assetTest extends TestCase
 {
     use TestTrait;
-    use ExecuteCommandTrait;
     use EventuallyConsistentTestTrait;
 
-    private static $commandFile = __DIR__ . '/../asset.php';
     private static $storage;
     private static $bucketName;
     private static $bucket;
@@ -53,9 +50,9 @@ class assetTest extends TestCase
     {
         $fileName = 'my-assets.txt';
         $dumpFilePath = 'gs://' . self::$bucketName . '/' . $fileName;
-        $output = $this->runCommand('export', [
-            'project' => self::$projectId,
-            'filePath' => $dumpFilePath,
+        $output = $this->runFunctionSnippet('export_assets', [
+            'projectId' => self::$projectId,
+            'dumpFilePath' => $dumpFilePath,
         ]);
         $assetFile = self::$bucket->object($fileName);
         $this->assertEquals($assetFile->name(), $fileName);
@@ -67,8 +64,8 @@ class assetTest extends TestCase
         $assetName = '//storage.googleapis.com/' . self::$bucketName;
 
         $this->runEventuallyConsistentTest(function () use ($assetName) {
-            $output = $this->runCommand('batch-get-history', [
-                'project' => self::$projectId,
+            $output = $this->runFunctionSnippet('batch_get_assets_history', [
+                'projectId' => self::$projectId,
                 'assetNames' => [$assetName],
             ]);
 
