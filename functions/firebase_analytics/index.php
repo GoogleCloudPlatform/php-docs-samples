d67f8e52c301df7223f930e9978b3ca3f2263fb5
ace-n <anassri@google.com>
2021-03-24 13:42:13 -0700

    Hide update data if it's not provided

--- functions/firebase_analytics/index.php
+++ functions/firebase_analytics/index.php
@@ -42,8 +42,12 @@ function firebaseAnalytics(CloudEvent $cloudevent)
     ));
 
     fwrite($log, 'Device Model: %s' . $userObj['deviceInfo']['deviceModel'] . PHP_EOL);
-    fwrite($log, 'Update type: ' . $data['updateType'] . PHP_EOL);
-    fwrite($log, 'Origin: ' . $data['updateOrigin'] . PHP_EOL);
-    fwrite($log, 'Version: ' . $data['versionNumber'] . PHP_EOL);
+
+    // Only print out update-related data if it is actually available
+    if (isset($data['updateType'])) {
+        fwrite($log, 'Update type: ' . $data['updateType'] . PHP_EOL);
+        fwrite($log, 'Origin: ' . $data['updateOrigin'] . PHP_EOL);
+        fwrite($log, 'Version: ' . $data['versionNumber'] . PHP_EOL);
+    }
 }
 // [END functions_firebase_analytics]
