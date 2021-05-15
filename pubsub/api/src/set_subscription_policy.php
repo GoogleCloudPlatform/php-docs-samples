c285a5b6899e441e0370f8905f81b2219a2bfbda
John Pedrie <john@pedrie.com>
2021-05-15 16:51:24 -0400

    update pubsub samples

--- pubsub/api/src/set_subscription_policy.php
+++ pubsub/api/src/set_subscription_policy.php
@@ -51,3 +51,5 @@ function set_subscription_policy($projectId, $subscriptionName, $userEmail)
         $subscriptionName);
 }
 # [END pubsub_set_subscription_policy]
+require_once __DIR__ . '/../../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
