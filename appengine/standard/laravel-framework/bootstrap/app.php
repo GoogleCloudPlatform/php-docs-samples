a4d0cec553d32a896fab16db6d6d60de589dfaa0
ace-n <anassri@google.com>
2021-05-17 18:21:28 -0700

    Fix Laravel compilation issue, take 2

--- appengine/standard/laravel-framework/bootstrap/app.php
+++ appengine/standard/laravel-framework/bootstrap/app.php
@@ -37,6 +37,11 @@ $app->singleton(
 );
 
 $app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
     Illuminate\Foundation\Exceptions\Handler::class,
     App\Exceptions\Handler::class
 );
