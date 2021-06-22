1be769386b13cb2ac89d9d29abbc8361520cda9a
Brent Shaffer <betterbrent@google.com>
2021-06-22 11:59:29 -0700

    ignore multiline formats for now

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,9 +8,9 @@ return (new PhpCsFixer\Config())
         'method_argument_space' => false,
         'whitespace_after_comma_in_array' => true,
         'method_argument_space' => [
-            'keep_multiple_spaces_after_comma' => true
+            'keep_multiple_spaces_after_comma' => true, // for wordpress constants
+            'on_multiline' => 'ignore', // consider removing this someday
         ],
-        'native_function_invocation' => true,
         'return_type_declaration' => [
             'space_before' => 'none'
         ],
