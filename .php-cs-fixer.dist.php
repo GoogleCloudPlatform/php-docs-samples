ad8a759007d7bc105216209d75355a751cfb3d6e
rsamborski <rsamborski@gmail.com>
2021-07-02 11:19:02 +0200

    Updating php-cs-fixer config file to support php-cs-fixer 3.0

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,24 +1,6 @@
 <?php
 
-$config = new PhpCsFixer\Config();
-$config
-    ->setRules([
-        '@PSR2' => true,
-        'concat_space' => ['spacing' => 'one'],
-        'no_unused_imports' => true,
-        'method_argument_space' => false,
-        'whitespace_after_comma_in_array' => true,
-        'method_argument_space' => [
-            'keep_multiple_spaces_after_comma' => true
-        ],
-        'return_type_declaration' => [
-            'space_before' => 'none'
-        ],
-    ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->in(__DIR__)
-    )
-;
+// Once we move completely to php-cs-fixer 3.0 we should make this file the only
+// distributed config file and remove `.php_cs.dist` file.
 
-return $config;
+return require '.php_cs.dist';
