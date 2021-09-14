01f0927f0d098314da5bb41e52e76fdb0987747d
rsamborski <rsamborski@gmail.com>
2021-09-14 23:10:23 +0200

    Refactoring compute_instances_operation_check sample
    - moved wait_for_operation to a separate file
    - added autoDelete flag to disks created with the instance to make sure they are cleaned up after VM deletion

--- compute/cloud-client/instances/src/wait_for_operation.php
+++ compute/cloud-client/instances/src/wait_for_operation.php
@@ -23,41 +23,40 @@
 
 namespace Google\Cloud\Samples\Compute;
 
-include_once "wait_for_operation.php";
-
-# [START compute_instances_delete]
-use Google\Cloud\Compute\V1\InstancesClient;
+# [START compute_instances_operation_check]
+use Google\Cloud\Compute\V1\Operation;
+use Google\Cloud\Compute\V1\ZoneOperationsClient;
 
 /**
- * Delete an instance.
- * Example:
- * ```
- * delete_instance($projectId, $zone, $instanceName);
- * ```
+ * This method waits for an operation to be completed. Calling this function
+ * will block until the operation is finished.
  *
+ * @param Operation $operation The Operation object representing the operation you want to
+wait on.
  * @param string $projectId Your Google Cloud project ID.
  * @param string $zone Zone where the instance you want to delete is (like "us-central1-a").
- * @param string $instanceName Unique name for the Compute instance to delete.
  *
  * @throws \Google\ApiCore\ApiException if the remote call fails.
+ * @return Operation Finished Operation object.
  */
-function delete_instance(
+function wait_for_operation(
+    Operation $operation,
     string $projectId,
-    string $zone,
-    string $instanceName
-) {
-    // Delete the Compute Engine instance using InstancesClient.
-    $instancesClient = new InstancesClient();
-    $operation = $instancesClient->delete($instanceName, $projectId, $zone);
+    string $zone
+): Operation {
+    $operationClient = new ZoneOperationsClient();
 
-    $operation = wait_for_operation($operation, $projectId, $zone);
-    if (! $operation->getError()) {
-        printf('Deleted instance %s' . PHP_EOL, $instanceName);
-    } else {
-        printf('Instance deletion failed!' . PHP_EOL);
+    while ($operation->getStatus() != Operation\Status::DONE) {
+        // Wait for the operation to complete.
+        $operation = $operationClient->wait($operation->getName(), $projectId, $zone);
+
+        if ($operation->hasError()) {
+            printf("Operation failed with error(s): %s" . PHP_EOL, $operation->getError()->serializeToString());
+            return $operation;
+        }
     }
-}
-# [END compute_instances_delete]
 
-require_once __DIR__ . '/../../../../testing/sample_helpers.php';
-\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
+    print("Operation successful" . PHP_EOL);
+    return $operation;
+}
+# [END compute_instances_operation_check]
