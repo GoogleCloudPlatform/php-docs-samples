260216f440116ab239d1d0e3eec2ec3deb2e48cb
Brent Shaffer <betterbrent@google.com>
2021-09-20 12:39:16 -0600

    Update asset/test/assetTest.php

--- asset/test/assetTest.php
+++ asset/test/assetTest.php
@@ -64,9 +64,9 @@ class assetTest extends TestCase
         $assetName = '//storage.googleapis.com/' . self::$bucketName;
         $this->runEventuallyConsistentTest(function () use ($assetName) {
             $output = $this->runFunctionSnippet('list_assets', [
-              'projectId' => self::$projectId,
-              'assetTypes' => ['storage.googleapis.com/Bucket'],
-              'pageSize' => 1000,
+                'projectId' => self::$projectId,
+                'assetTypes' => ['storage.googleapis.com/Bucket'],
+                'pageSize' => 1000,
             ]);
 
             $this->assertStringContainsString($assetName, $output);
