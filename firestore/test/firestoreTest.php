420fd73dae0421c06d9da7cf641b8902c67b356c
Brent Shaffer <betterbrent@google.com>
2021-09-23 14:05:07 -0600

    Update firestore/test/firestoreTest.php

--- firestore/test/firestoreTest.php
+++ firestore/test/firestoreTest.php
@@ -75,7 +75,7 @@ class firestoreTest extends TestCase
     public function testInitialize()
     {
         $output = self::runFunctionSnippet('setup_client_create', [self::$projectId]);
-        $this->assertStringContainsString('Created Cloud Firestore client with default project ID.', $output);
+        $this->assertStringContainsString('Created Cloud Firestore client with project ID: ', $output);
     }
 
     public function testInitializeProjectId()
