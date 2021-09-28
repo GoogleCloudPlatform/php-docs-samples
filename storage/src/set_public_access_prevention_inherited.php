4c67e0c7aa76216ea557f3e22dee9b763d5d99da
saranshdhingra <saranshdhingra@google.com>
2021-09-28 18:50:19 +0530

    Storage: Changed references from 'unspecified' to 'inherited'

--- storage/src/set_public_access_prevention_inherited.php
+++ storage/src/set_public_access_prevention_inherited.php
@@ -23,15 +23,15 @@
 
 namespace Google\Cloud\Samples\Storage;
 
-# [START storage_set_public_access_prevention_unspecified]
+# [START storage_set_public_access_prevention_inherited]
 use Google\Cloud\Storage\StorageClient;
 
 /**
- * Set the bucket Public Access Prevention to unspecified.
+ * Set the bucket Public Access Prevention to inherited.
  *
  * @param string $bucketName the name of your Cloud Storage bucket.
  */
-function set_public_access_prevention_unspecified($bucketName)
+function set_public_access_prevention_inherited($bucketName)
 {
     // $bucketName = 'my-bucket';
 
@@ -40,16 +40,16 @@ function set_public_access_prevention_unspecified($bucketName)
 
     $bucket->update([
         'iamConfiguration' => [
-            'publicAccessPrevention' => 'unspecified'
+            'publicAccessPrevention' => 'inherited'
         ]
     ]);
 
     printf(
-        'Public Access Prevention has been set to unspecified for %s.' . PHP_EOL,
+        'Public Access Prevention has been set to inherited for %s.' . PHP_EOL,
         $bucketName
     );
 }
-# [END storage_set_public_access_prevention_unspecified]
+# [END storage_set_public_access_prevention_inherited]
 
 // The following 2 lines are only needed to run the samples
 require_once __DIR__ . '/../../testing/sample_helpers.php';
