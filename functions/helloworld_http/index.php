b6d66b5afa1e6c736870e820841087df4b271cd3
Brent Shaffer <betterbrent@google.com>
2021-10-14 15:04:06 -0600

    fix: HTML region tag (#1541)

--- functions/helloworld_http/index.php
+++ functions/helloworld_http/index.php
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 
-// [START functions_helloworld_http]
+<!-- [START functions_helloworld_http] ->
 <?php
 
 use Psr\Http\Message\ServerRequestInterface;
