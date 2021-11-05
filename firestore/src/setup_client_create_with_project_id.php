9f190550437cfb3f330a5cff3233fad247cd80d2
Craig Labenz <craig.labenz@gmail.com>
2021-11-05 12:04:17 -0700

    docs: consolidated client creation snippets (#1447)

--- firestore/src/setup_client_create_with_project_id.php
+++ firestore/src/setup_client_create_with_project_id.php
@@ -23,6 +23,8 @@
 
 namespace Google\Cloud\Samples\Firestore;
 
+# TODO(craiglabenz): Remove the `firestore_setup_client_create_with_project_id`
+#                    region tag after consolidating to `firestore_setup_client_create`
 # [START fs_initialize_project_id]
 # [START firestore_setup_client_create_with_project_id]
 use Google\Cloud\Firestore\FirestoreClient;
@@ -42,6 +44,8 @@ function setup_client_create_with_project_id(string $projectId): void
 }
 # [END firestore_setup_client_create_with_project_id]
 # [END fs_initialize_project_id]
+# TODO(craiglabenz): Remove the `firestore_setup_client_create_with_project_id`
+#                    region tag after consolidating to `firestore_setup_client_create`
 
 // The following 2 lines are only needed to run the samples
 require_once __DIR__ . '/../../testing/sample_helpers.php';
