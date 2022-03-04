7bee8023a0d9a9917065d74310f0a2a0043eb39f
Saransh Dhingra <saranshdhingra@google.com>
2022-03-04 09:59:26 +0000

    feat(spanner): Added 8 samples for PG dialect

---
+++ spanner/src/pg_spanner_create_database.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * For instructions on how to run the full sample:
+ *
+ * @see https://github.com/GoogleCloudPlatform/php-docs-samples/tree/master/spanner/README.md
+ */
+
+namespace Google\Cloud\Samples\Spanner;
+
+// [START spanner_create_postgres_database]
+use Google\Cloud\Spanner\SpannerClient;
+use Google\Cloud\Spanner\Admin\Database\V1\DatabaseDialect;
+
+/**
+ * Creates a database that uses Postgres dialect
+ *
+ * @param string $instanceId The Spanner instance ID.
+ * @param string $databaseId The Spanner database ID.
+ */
+function pg_spanner_create_database(string $instanceId, string $databaseId): void {
+    $spanner = new SpannerClient();
+    $instance = $spanner->instance($instanceId);
+
+    if (!$instance->exists()) {
+        throw new \LogicException("Instance $instanceId does not exist");
+    }
+
+    $operation = $instance->createDatabase($databaseId, [
+        'databaseDialect' => DatabaseDialect::POSTGRESQL
+    ]);
+
+    print('Waiting for operation to complete...' . PHP_EOL);
+    $operation->pollUntilComplete();
+
+    $database = $instance->database($databaseId);
+    $dialect = DatabaseDialect::name($database->info()['databaseDialect']);
+
+    printf('Created database %s with dialect %s on instance %s' . PHP_EOL,
+        $databaseId, $dialect, $instanceId);
+}
+// [END spanner_create_database]
+
+// The following 2 lines are only needed to run the samples
+require_once __DIR__ . '/../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
