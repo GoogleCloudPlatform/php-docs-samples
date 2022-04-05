56329e310ab3b9f80ffb6bcea85db384808792bd
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2022-04-05 20:17:49 +0530

    feat: [Storage] complex upload download samples (#1606)

--- storage/src/download_byte_range.php
+++ storage/src/download_byte_range.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2016 Google Inc.
+ * Copyright 2022 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,27 +23,41 @@
 
 namespace Google\Cloud\Samples\Storage;
 
-# [START storage_download_file]
-# [START storage_stream_file_download]
+# [START storage_download_byte_range]
 use Google\Cloud\Storage\StorageClient;
 
 /**
- * Download an object from Cloud Storage and save it as a local file.
+ * Download a byte range from Cloud Storage and save it as a local file.
  *
  * @param string $bucketName The name of your Cloud Storage bucket.
  * @param string $objectName The name of your Cloud Storage object.
+ * @param int $startByte The starting byte at which to begin the download.
+ * @param int $endByte The ending byte at which to end the download.
  * @param string $destination The local destination to save the object.
  */
-function download_object($bucketName, $objectName, $destination)
-{
+function download_byte_range(
+    string $bucketName,
+    string $objectName,
+    int $startByte,
+    int $endByte,
+    string $destination
+): void {
     // $bucketName = 'my-bucket';
     // $objectName = 'my-object';
+    // $startByte = 1;
+    // $endByte = 5;
     // $destination = '/path/to/your/file';
 
     $storage = new StorageClient();
     $bucket = $storage->bucket($bucketName);
     $object = $bucket->object($objectName);
-    $object->downloadToFile($destination);
+    $object->downloadToFile($destination, [
+        'restOptions' => [
+            'headers' => [
+                'Range' => "bytes=$startByte-$endByte",
+            ],
+        ],
+    ]);
     printf(
         'Downloaded gs://%s/%s to %s' . PHP_EOL,
         $bucketName,
@@ -51,8 +65,7 @@ function download_object($bucketName, $objectName, $destination)
         basename($destination)
     );
 }
-# [END storage_stream_file_download]
-# [END storage_download_file]
+# [END storage_download_byte_range]
 
 // The following 2 lines are only needed to run the samples
 require_once __DIR__ . '/../../testing/sample_helpers.php';
