988f9f2c754e2938ae5f8a418e2ab033d37f29bb
Anwesha <anweshanaskar@gmail.com>
2022-08-24 17:30:59 +0000

    Fixes linting issues

--- analyticsdata/run_report.php
+++ analyticsdata/run_report.php
@@ -37,9 +37,7 @@ require 'vendor/autoload.php';
 use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
 use Google\Analytics\Data\V1beta\DateRange;
 use Google\Analytics\Data\V1beta\Dimension;
-use Google\Analytics\Data\V1beta\DimensionHeader;
 use Google\Analytics\Data\V1beta\Metric;
-use Google\Analytcs\Data\V1beta\MetricHeader;
 use Google\Analytics\Data\V1beta\MetricType;
 
 function runReport()
@@ -87,13 +85,13 @@ function runReport()
 function printRunReportResponse($response)
 {
     // [START analyticsdata_print_run_report_response_header]
-    printf("%s rows received%s", $response->getRowCount(), PHP_EOL);
+    printf('%s rows received%s', $response->getRowCount(), PHP_EOL);
     foreach ($response->getDimensionHeaders() as $dimensionHeader) {
-        printf("Dimension header name: %s%s", $dimensionHeader->getName(), PHP_EOL);
+        printf('Dimension header name: %s%s', $dimensionHeader->getName(), PHP_EOL);
     }
     foreach ($response->getMetricHeaders() as $metricHeader) {
         printf(
-            "Metric header name: %s (%s)%s",
+            'Metric header name: %s (%s)%s',
             $metricHeader->getName(),
             MetricType::name($metricHeader->getType()),
             PHP_EOL
