cfc5dad8da58c11027c84444ce15abb2295d3756
yash30201 <54198301+yash30201@users.noreply.github.com>
2022-09-28 15:29:30 +0530

    Working on sample implementation

---
+++ storage/src/print_bucket_website_configuration.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * For instructions on how to run the full sample:
+ *
+ * @see https://github.com/GoogleCloudPlatform/php-docs-samples/tree/master/storage/README.md
+ */
+
+namespace Google\Cloud\Samples\Storage;
+
+# [START storage_print_bucket_website_configuration]
+use Google\Cloud\Storage\StorageClient;
+
+/**
+ * Print an entity role for a bucket ACL.
+ *
+ * @param string $bucketName The name of your Cloud Storage bucket.
+ * @param string $entity The entity for which to query access controls.
+ */
+function print_bucket_website_configuration(
+    string $bucketName,
+): void {
+
+    $storage = new StorageClient();
+    $bucket = $storage->bucket($bucketName);
+    $info = $bucket->info();
+
+    if(!array_key_exists('website', $info)) {
+      printf('Bucket website configuration not set');
+    } else {
+      printf(
+        'Main page suffix: %s' . PHP_EOL . 'Not found page: %s',
+        $info['website']['mainPageSuffix'],
+        $info['website']['notFoundPage'],
+      );
+    }
+}
+# [END storage_print_bucket_website_configuration]
+
+// The following 2 lines are only needed to run the samples
+require_once __DIR__ . '/../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
