2095eb72830c90cae657ac37997c4382cfe7587a
Ajumal <ajumalpa@google.com>
2022-10-03 13:13:03 +0530

    Add return types and signature arguments for spanner samples.

--- spanner/src/write_data_with_dml.php
+++ spanner/src/write_data_with_dml.php
@@ -40,7 +40,7 @@ use Google\Cloud\Spanner\Transaction;
  * @param string $instanceId The Spanner instance ID.
  * @param string $databaseId The Spanner database ID.
  */
-function write_data_with_dml($instanceId, $databaseId)
+function write_data_with_dml(string $instanceId, string $databaseId): void
 {
     $spanner = new SpannerClient();
     $instance = $spanner->instance($instanceId);
