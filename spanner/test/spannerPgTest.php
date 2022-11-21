9450de4179105836fdde0e35f4a0b1a7a1394481
Yash Sahu <54198301+yash30201@users.noreply.github.com>
2022-11-21 22:37:32 +0530

    feat(Spanner): Added DML returning samples (#1721)
    * Spanner: Added samples for DML returning.

--- spanner/test/spannerPgTest.php
+++ spanner/test/spannerPgTest.php
@@ -363,6 +363,50 @@ class spannerPgTest extends TestCase
         $this->assertStringContainsString('Marketing budget updated.', $output);
     }
 
+    /**
+     * @depends testCreateDatabase
+     */
+    public function testDmlReturningInsert()
+    {
+        $output = $this->runFunctionSnippet('pg_insert_dml_returning');
+
+        $expectedOutput = sprintf('Row (16, Melissa, Garcia) inserted');
+        $this->assertStringContainsString($expectedOutput, $output);
+
+        $expectedOutput = sprintf('Row (17, Russell, Morales) inserted');
+        $this->assertStringContainsString('Russell', $output);
+
+        $expectedOutput = sprintf('Row (18, Jacqueline, Long) inserted');
+        $this->assertStringContainsString('Jacqueline', $output);
+
+        $expectedOutput = sprintf('Row (19, Dylan, Shaw) inserted');
+        $this->assertStringContainsString('Dylan', $output);
+    }
+
+    /**
+     * @depends testDmlReturningInsert
+     */
+    public function testDmlReturningUpdate()
+    {
+        $output = $this->runFunctionSnippet('pg_update_dml_returning');
+
+        $expectedOutput = sprintf(
+            'Row with singerid 16 updated to (16, Melissa, Missing)'
+        );
+        $this->assertStringContainsString($expectedOutput, $output);
+    }
+
+    /**
+     * @depends testDmlReturningUpdate
+     */
+    public function testDmlReturningDelete()
+    {
+        $output = $this->runFunctionSnippet('pg_delete_dml_returning');
+
+        $expectedOutput = sprintf('Row (16, Melissa, Missing) deleted');
+        $this->assertStringContainsString($expectedOutput, $output);
+    }
+
     public static function tearDownAfterClass(): void
     {
         // Clean up
