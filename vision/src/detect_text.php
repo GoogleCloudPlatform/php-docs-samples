30b7eba83fb9fb1ac9cb91f76f31a1dcf5cb1d9f
Brent Shaffer <betterbrent@google.com>
2022-11-29 14:19:03 -0800

    chore: upgrade vision samples to new format (#1638)

--- vision/src/detect_text.php
+++ vision/src/detect_text.php
@@ -20,9 +20,10 @@ namespace Google\Cloud\Samples\Vision;
 
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
-// $path = 'path/to/your/image.jpg';
-
-function detect_text($path)
+/**
+ * @param string $path Path to the image, e.g. "path/to/your/image.jpg"
+ */
+function detect_text(string $path)
 {
     $imageAnnotator = new ImageAnnotatorClient();
 
@@ -47,3 +48,7 @@ function detect_text($path)
     $imageAnnotator->close();
 }
 // [END vision_text_detection]
+
+// The following 2 lines are only needed to run the samples
+require_once __DIR__ . '/../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
