9077e077865fe74e79e2471d21235e9a21856bef
Anwesha <anweshanaskar@gmail.com>
2022-12-15 00:27:11 +0000

    Readability linting changes

--- analyticsdata/src/run_report_with_date_ranges.php
+++ analyticsdata/src/run_report_with_date_ranges.php
@@ -90,10 +90,9 @@ function printRunReportResponseWithDateRanges(RunReportResponse $response)
     }
     foreach ($response->getMetricHeaders() as $metricHeader) {
         printf(
-            'Metric header name: %s (%s)%s',
+            'Metric header name: %s (%s)' . PHP_EOL,
             $metricHeader->getName(),
-            MetricType::name($metricHeader->getType()),
-            PHP_EOL
+            MetricType::name($metricHeader->getType())
         );
     }
     // [END analyticsdata_print_run_report_response_header]
@@ -102,8 +101,11 @@ function printRunReportResponseWithDateRanges(RunReportResponse $response)
     print 'Report result: ' . PHP_EOL;
 
     foreach ($response->getRows() as $row) {
-        print $row->getDimensionValues()[0]->getValue()
-        . ' ' . $row->getMetricValues()[0]->getValue() . PHP_EOL;
+        printf(
+            '%s %s' . PHP_EOL,
+            $row->getDimensionValues()[0]->getValue(),
+            $row->getMetricValues()[0]->getValue()
+        );
     }
     // [END analyticsdata_print_run_report_response_rows]
 }
