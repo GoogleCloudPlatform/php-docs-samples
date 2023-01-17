3c63dd1fb08ac8f2f0d140de28ff6ac39390a80a
Holt Skinner <holtskinner@google.com>
2023-01-17 11:02:47 -0600

    docs(samples): Addressed linter error 'quotation marks'

--- documentai/quickstart.php
+++ documentai/quickstart.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2023 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -28,33 +28,33 @@ use Google\Cloud\DocumentAI\V1\RawDocument;
 $client = new DocumentProcessorServiceClient();
 
 # Local File Path
-$documentPath = "resources/invoice.pdf";
+$documentPath = 'resources/invoice.pdf';
 
 # Read in File Contents
-$handle = fopen($documentPath, "rb");
+$handle = fopen($documentPath, 'rb');
 $contents = fread($handle, filesize($documentPath));
 fclose($handle);
 
 # Load File Contents into RawDocument
 $rawDocument = new RawDocument([
-    "content" => $contents,
-    "mime_type" => "application/pdf"
+    'content' => $contents,
+    'mime_type' => 'application/pdf'
 ]);
 
-$projectId = "YOUR_PROJECT_ID"; # Your Google Cloud Platform project ID
-$location = "us"; # Your Processor Location
-$processor = "YOUR_PROCESSOR_ID"; # Your Processor ID
+$projectId = 'YOUR_PROJECT_ID'; # Your Google Cloud Platform project ID
+$location = 'us'; # Your Processor Location
+$processor = 'YOUR_PROCESSOR_ID'; # Your Processor ID
 
 # Fully-qualified Processor Name
 $name = $client->processorName([
-    "project" => $projectId,
-    "location" => $location,
-    "processor" => $processor
+    'project' => $projectId,
+    'location' => $location,
+    'processor' => $processor
 ]);
 
 $processRequest = new ProcessRequest([
-    "name" => $name,
-    "raw_document" => $rawDocument
+    'name' => $name,
+    'raw_document' => $rawDocument
 ]);
 
 # Make Processing Request
