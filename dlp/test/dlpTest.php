213a7d5738001a56d191057800017a0738c689a2
sameershaikh_crest <sameer.shaikh@crestdatasys.com>
2023-04-04 12:04:41 +0530

    dlp_deidentify_simple_word_list

--- dlp/test/dlpTest.php
+++ dlp/test/dlpTest.php
@@ -15,9 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-namespace Google\Cloud\TestUtils;
-
 namespace Google\Cloud\Samples\Dlp;
 
 use Google\Cloud\TestUtils\TestTrait;
