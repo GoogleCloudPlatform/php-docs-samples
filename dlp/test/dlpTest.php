1dab3cb3d759d0dcf60e1809c53b779ff98c19d7
sameer-crest <129392897+sameer-crest@users.noreply.github.com>
2023-04-19 21:11:26 +0530

    feat(dlp): inspect a string for sensitive data, omitting overlapping matches on domain and email (#1805)

--- dlp/test/dlpTest.php
+++ dlp/test/dlpTest.php
@@ -259,6 +259,17 @@ class dlpTest extends TestCase
         $this->assertStringContainsString('Successfully deleted job ' . $jobId, $output);
     }
 
+    public function testInspectStringWithoutOverlap()
+    {
+        $output = $this->runFunctionSnippet('inspect_string_without_overlap', [
+            self::$projectId,
+            'example.com is a domain, james@example.org is an email.'
+        ]);
+
+        $this->assertStringContainsString('Info type: DOMAIN_NAME', $output);
+        $this->assertStringNotContainsString('Info type: EMAIL_ADDRESS', $output);
+    }
+
     public function testInspectStringWithExclusionDict()
     {
         $output = $this->runFunctionSnippet('inspect_string_with_exclusion_dict', [
