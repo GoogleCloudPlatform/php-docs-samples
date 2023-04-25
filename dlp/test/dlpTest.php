77ccdef591abe594f169a5193ae4609d9ce30a6c
sameershaikh_crest <sameer.shaikh@crestdatasys.com>
2023-04-25 11:31:12 +0530

    Implemented dlp_inspect_string_with_exclusion_dict_substring

--- dlp/test/dlpTest.php
+++ dlp/test/dlpTest.php
@@ -258,4 +258,15 @@ class dlpTest extends TestCase
         );
         $this->assertStringContainsString('Successfully deleted job ' . $jobId, $output);
     }
+
+    public function testInspectStringWithExclusionDictSubstring()
+    {
+        $output = $this->runFunctionSnippet('inspect_string_with_exclusion_dict_substring', [
+            self::$projectId,
+            'Some email addresses: gary@example.com, TEST@example.com'
+        ]);
+        $this->assertStringContainsString('Quote: example.com', $output);
+        $this->assertStringContainsString('Info type: EMAIL_ADDRESS', $output);
+        $this->assertStringNotContainsString('TEST@example.com', $output);
+    }
 }
