09c509a4117173a5eb7aa2c354d35eab73a86c27
janell-chen <122311137+janell-chen@users.noreply.github.com>
2023-06-14 17:49:04 -0700

    Address comments

--- functions/response_streaming/index.php
+++ functions/response_streaming/index.php
@@ -24,28 +24,15 @@ function streamBigQuery(ServerRequestInterface $request)
 {
     // Provide the Cloud Project ID by setting env var.
     $projectId = getenv('GOOGLE_PROJECT_ID');
-    $datasetId = 'my_new_dataset_' . time();
+    
     // Example large payload from BigQuery's public dataset.
-    $query = 'SELECT abstract FROM `bigquery-public-data.breathe.bioasq` LIMIT 1000';
-
-    $bigQuery = new BigQueryClient([
-        'projectId' => $projectId,
-        'datasetId' => $datasetId,
-    ]);
-    $jobConfig = $bigQuery->query($query);
-    $job = $bigQuery->startQuery($jobConfig);
-
-    $backoff = new ExponentialBackoff(10);
-    $backoff->execute(function () use ($job) {
-        $job->reload();
-        if (!$job->isComplete()) {
-            throw new Exception('Job has not yet completed', 500);
-        }
-    });
-    $queryResults = $job->queryResults();
-
+    $bigQuery = new BigQueryClient(['projectId' => $projectId]);
+    $queryJobConfig = $bigQuery->query(
+        'SELECT abstract FROM `bigquery-public-data.breathe.bioasq` LIMIT 1000'
+        );
+    $queryResults = $bigQuery->runQuery($queryJobConfig);
+    
     // Stream out large payload by iterating rows and flushing output.
-    $i = 0;
     foreach ($queryResults as $row) {
         foreach ($row as $column => $value) {
             printf('%s' . PHP_EOL, json_encode($value));
