508434d547e0123c4c64093fa37e7530e4c88ecf
pattishin@google.com <pattishin@users.noreply.github.com>
2023-07-31 11:18:41 -0700

    test: update php mc test and check deployment logs

--- run/multi-container/hello-php-nginx-sample/test/MCNGinxTest.php
+++ run/multi-container/hello-php-nginx-sample/test/MCNGinxTest.php
@@ -17,13 +17,10 @@
 
 namespace Google\Cloud\Samples\Run\MCHelloPHPNginx;
 
-use Google\Auth\ApplicationDefaultCredentials;
 use Google\Cloud\TestUtils\DeploymentTrait;
 use Google\Cloud\TestUtils\EventuallyConsistentTestTrait;
 use Google\Cloud\TestUtils\GcloudWrapper\CloudRun;
 use Google\Cloud\TestUtils\TestTrait;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -36,43 +33,82 @@ class MCNginxTest extends TestCase
     use EventuallyConsistentTestTrait;
     use TestTrait;
 
-    /** @var \Google\Cloud\TestUtils\GcloudWrapper\CloudRun */
-    private static $nginxService;
-    private static $phpService;
-
     /** @var string */
     private static $projectId;
     private static $region;
-    private static $versionId;
     private static $repoName;
+    private static $mcServiceName;
+    private static $mcService;
     private static $nginxImage;
     private static $appImage;
 
+
     /**
-     * Set up Artifact Registry image tags refs and declare Cloud Run services
+     * Execute given cmd
+     *
+     * Note: The use of exec() outside of the usual CloudRun stub class deploy() pattern is intentional
+     * as the feature is in public preview. If more php multi-container samples get added, this should be
+     * refactored to make `gcloud run services replace ...` reusable across samples
+     */
+    private static function execCmd($cmd = '', $output = null, $retvalue = 3)
+    {
+        if (false === exec($cmd, $output, $retvalue)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Set up test leveraged variabled and establish Artifact Registry image tags refs
      */
     public static function setUpDeploymentVars()
     {
-        $projectId = self::requireEnv('GOOGLE_PROJECT_ID');
-        $region = getenv('REGION') ?: 'us-west1';
-        $repoName = getenv('REPO_NAME') ?: 'cloud-run-source-deploy';
-        $versionId = getenv('GOOGLE_VERSION_ID') ?: sprintf('hellophpnginx-%s', time());
+        $timestamp = time();
+        self::$projectId = self::requireEnv('GOOGLE_PROJECT_ID');
+        self::$region = getenv('REGION') ?: 'us-west1';
+        self::$repoName = getenv('REPO_NAME') ?: 'cloud-run-source-deploy';
+        self::$mcServiceName = sprintf('hello-php-nginx-mc-%s', $timestamp);
+        $versionId = getenv('GOOGLE_VERSION_ID') ?: self::$mcServiceName;
 
-        // Declaring Cloud Run services
-        self::$nginxService = new CloudRun(self::$projectId, ['service' => $versionId]);
-        self::$phpService = new CloudRun(self::$projectId, ['service' => $versionId]);
+        self::$mcService = new CloudRun(self::$projectId, ['service' => $versionId]);
 
         // Declaring Cloud Build image tags
-        self::$nginxImage = sprintf('%s-docker.pkg.dev/%s/%s/nginx', $region, self::$projectId, $repoName);
-        self::$appImage = sprintf('%s-docker.pkg.dev/%s/%s/php', $region, self::$projectId, $repoName);
+        self::$nginxImage = sprintf('%s-docker.pkg.dev/%s/%s/nginx', self::$region, self::$projectId, self::$repoName);
+        self::$appImage = sprintf('%s-docker.pkg.dev/%s/%s/php', self::$region, self::$projectId, self::$repoName);
     }
 
     private static function beforeDeploy()
     {
         self::setUpDeploymentVars();
+        self::buildImages();
+        self::doYamlSubstitution();
 
         // Suppress gcloud prompts during deployment.
         putenv('CLOUDSDK_CORE_DISABLE_PROMPTS=1');
+    }
+
+    /**
+     * Build nginx + hello php container images
+     */
+    private static function buildImages()
+    {
+        if (false === self::$mcService->build(self::$nginxImage, [], './nginx')) {
+            return false;
+        }
+        if (false === self::$mcService->build(self::$appImage, [], './php-app')) {
+            return false;
+        }
+    }
+
+    /**
+     * Execute yaml substitution
+     */
+    private static function doYamlSubstitution()
+    {
+        // Execute yaml substitution
+        $subCmd = sprintf('sed -i -e s/MC_SERVICE_NAME/%s/g -e s/REGION/%s/g -e s/REPO_NAME/%s/g -e s/PROJECT_ID/%s/g service.yaml', self::$mcServiceName, self::$region, self::$repoName, self::$projectId);
+
+        return self::execCmd($subCmd);
     }
 
     /**
@@ -80,63 +116,60 @@ class MCNginxTest extends TestCase
      */
     private static function doDeploy()
     {
-        // Build & deploy nginx container image
-        if (false === self::$nginxService->build(self::$nginxImage, [], '../nginx')) {
-            return false;
-        }
-        if (false === self::$nginxService->deploy(self::$nginxImage)) {
+        // Execute multi-container service deployment
+        $mcCmd = sprintf('gcloud run services replace service.yaml --region %s --quiet', self::$region);
+
+        return self::execCmd($mcCmd);
+    }
+
+    /**
+     * Delete a deployed Cloud Run MC service and related images.
+     */
+    private static function doDelete()
+    {
+        self::$mcService->delete();
+        self::$mcService->deleteImage(self::$nginxImage);
+        self::$mcService->deleteImage(self::$appImage);
+    }
+
+    /**
+     * Test that the multi-container is running with both
+     * serving and sidecar running as expected
+     */
+    public function testService()
+    {
+        $mcUrl = self::execCmd(sprintf('gcloud run services describe %s --region %s --format "value(status.url)"', self::$mcServiceName, self::$region));
+        $mcStatus = self::execCmd(sprintf('gcloud run services describe %s --region %s --format "value(status.conditions[0].type)"', self::$mcServiceName, self::$region));
+
+        echo "=====url=====";
+        echo "${mcUrl}";
+        echo "${mcStatus}";
+        echo "==========";
+
+        if (empty($mcUrl) or empty($mcStatus)) {
             return false;
         }
 
-        // Build & deploy php app container image
-        if (false === self::$phpService->build(self::$appImage, [], '../php-app')) {
-            return false;
-        }
-        if (false === self::$phpService->deploy(self::$appImage)) {
+        # Checking that fpm (FastCGI Process Manager)
+        $mcNginxLog = exec('gcloud logging read "resource.type=cloud_run_revision AND resource.labels.service_name=%s AND labels.container_name=nginx" | grep -e "Default STARTUP TCP probe succeeded after 1 attempt for container nginx"', self::$mcServiceName);
+        # Check Cloud Run MC nginx & hellophp logs for signs of successful deployment and connection
+        $mcHelloPHPLog = exec('gcloud logging read "resource.type=cloud_run_revision AND resource.labels.service_name=%s AND labels.container_name=hellophp" | grep -e "NOTICE: fpm is running, pid 1"', self::$mcServiceName);
+
+        echo "=====log=====";
+        echo "${mcNginxLog}";
+        echo "${mcHelloPHPLog}";
+        echo "==========";
+
+        // Validate that the expected logs have been recovered
+        if (empty($mcNginxLog) or empty($mcHelloPHPLog)) {
             return false;
         }
 
         return true;
     }
 
-    /**
-     * Delete a deployed Cloud Run service.
-     */
-    private static function doDelete()
-    {
-        // Delete nginx Cloud Run service
-        self::$nginxService->delete();
-        self::$nginxService->deleteImage(self::$nginxImage);
-
-        // Delete hello php Cloud Run service
-        self::$phpService->delete();
-        self::$phpService->deleteImage(self::$appImage);
-    }
-
-    public function testService()
-    {
-        $targetAudience = self::getBaseUri();
-
-        // create middleware
-        $middleware = ApplicationDefaultCredentials::getIdTokenMiddleware($targetAudience);
-        $stack = HandlerStack::create();
-        $stack->push($middleware);
-
-        // create the HTTP client
-        $client = new Client([
-            'handler' => $stack,
-            'auth' => 'google_auth',
-            'base_uri' => $targetAudience,
-        ]);
-
-        // Run the test.
-        $resp = $client->get('/');
-        $this->assertEquals('200', $resp->getStatusCode());
-        $this->assertEquals('Hello World!', (string) $resp->getBody());
-    }
-
     public function getBaseUri()
     {
-        return self::$phpService->getBaseUrl();
+        return self::$mcService->getBaseUrl();
     }
 }
