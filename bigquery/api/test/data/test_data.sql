c0c61c2a38db64bab5ae6b3be9dbb079444cba57
RB-21 <61182651+RB-21@users.noreply.github.com>
2024-02-05 07:05:14 +0700

    first commit

--- bigquery/api/test/data/test_data.sql
+++
@@ -1,8 +0,0 @@
--- This file is used to test ../../src/insert_sql.php
--- These are comments.
--- Each query to be executed should be on a single line.
-
-/* Another ignored line. */
-INSERT INTO `test_table` (`name`, `title`) VALUES ('Brent Shaffer', 'PHP Developer')
-INSERT INTO `test_table` (`name`, `title`) VALUES ('Takashi Matsuo', 'Developer\'s Liberation Activist')
-INSERT INTO `test_table` (`name`, `title`) VALUES ('Jeffrey Rennie', 'The Man, The Myth, The Legend')
