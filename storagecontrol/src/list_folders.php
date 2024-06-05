3f4225e6968c8b418fd9c3289f415962bdf59746
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2024-06-05 22:27:59 +0000

    feat(storagecontrol): add HNS folders samples (#2013)

---
+++ storagecontrol/src/list_folders.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * For instructions on how to run the full sample:
+ *
+ * @see https://github.com/GoogleCloudPlatform/php-docs-samples/tree/main/storage/README.md
+ */
+
+namespace Google\Cloud\Samples\StorageControl;
+
+# [START storage_control_list_folders]
+use Google\Cloud\Storage\Control\V2\Client\StorageControlClient;
+use Google\Cloud\Storage\Control\V2\ListFoldersRequest;
+
+/**
+ * List folders in an existing bucket.
+ *
+ * @param string $bucketName The name of your Cloud Storage bucket.
+ *        (e.g. 'my-bucket')
+ */
+function list_folders(string $bucketName): void
+{
+    $storageControlClient = new StorageControlClient();
+
+    // Set project to "_" to signify global bucket
+    $formattedName = $storageControlClient->bucketName('_', $bucketName);
+
+    $request = new ListFoldersRequest([
+        'parent' => $formattedName,
+    ]);
+
+    $folders = $storageControlClient->listFolders($request);
+
+    foreach ($folders as $folder) {
+        printf('Folder name: %s' . PHP_EOL, $folder->getName());
+    }
+}
+# [END storage_control_list_folders]
+
+// The following 2 lines are only needed to run the samples
+require_once __DIR__ . '/../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
