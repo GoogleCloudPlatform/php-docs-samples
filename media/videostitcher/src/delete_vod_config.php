09b6fdfc3e41b4e42ef072bbdec50627cf1086b5
Nicholas Cook <irataxy@users.noreply.github.com>
2024-08-26 14:45:46 -0700

    feat(VideoStitcher): add samples and test for VOD config; update VOD session creation (#2042)

---
+++ media/videostitcher/src/delete_vod_config.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright 2024 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * For instructions on how to run the samples:
+ *
+ * @see https://github.com/GoogleCloudPlatform/php-docs-samples/tree/main/media/videostitcher/README.md
+ */
+
+namespace Google\Cloud\Samples\Media\Stitcher;
+
+// [START videostitcher_delete_vod_config]
+use Google\Cloud\Video\Stitcher\V1\Client\VideoStitcherServiceClient;
+use Google\Cloud\Video\Stitcher\V1\DeleteVodConfigRequest;
+
+/**
+ * Deletes a VOD config.
+ *
+ * @param string $callingProjectId     The project ID to run the API call under
+ * @param string $location             The location of the VOD config
+ * @param string $vodConfigId         The ID of the VOD config
+ */
+function delete_vod_config(
+    string $callingProjectId,
+    string $location,
+    string $vodConfigId
+): void {
+    // Instantiate a client.
+    $stitcherClient = new VideoStitcherServiceClient();
+
+    $formattedName = $stitcherClient->vodConfigName($callingProjectId, $location, $vodConfigId);
+    $request = (new DeleteVodConfigRequest())
+        ->setName($formattedName);
+    $operationResponse = $stitcherClient->deleteVodConfig($request);
+    $operationResponse->pollUntilComplete();
+    if ($operationResponse->operationSucceeded()) {
+        // Print status
+        printf('Deleted VOD config %s' . PHP_EOL, $vodConfigId);
+    } else {
+        $error = $operationResponse->getError();
+        // handleError($error)
+    }
+}
+// [END videostitcher_delete_vod_config]
+
+// The following 2 lines are only needed to run the samples
+require_once __DIR__ . '/../../../testing/sample_helpers.php';
+\Google\Cloud\Samples\execute_sample(__FILE__, __NAMESPACE__, $argv);
