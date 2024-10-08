8b4abf7f01d56ffcdc75c2f96a930ae64f86da4e
SarthakAjmera26 <ajmerasarthak@gmail.com>
2024-10-08 21:19:15 +0530

    chore: use newer runtimes and let nginx serve static files (#2058)

--- compute/firewall/src/print_firewall_rule.php
+++ compute/firewall/src/print_firewall_rule.php
@@ -54,9 +54,9 @@ function print_firewall_rule(string $projectId, string $firewallRuleName)
     printf('Self Link: %s' . PHP_EOL, $response->getSelfLink());
     printf('Logging Enabled: %s' . PHP_EOL, var_export($response->getLogConfig()->getEnable(), true));
     print('--Allowed--' . PHP_EOL);
-    foreach ($response->getAllowed() as $item) {
+    foreach ($response->getAllowed()as $item) {
         printf('Protocol: %s' . PHP_EOL, $item->getIPProtocol());
-        foreach ($item->getPorts()as $ports) {
+        foreach ($item->getPorts() as $ports) {
             printf(' - Ports: %s' . PHP_EOL, $ports);
         }
     }
