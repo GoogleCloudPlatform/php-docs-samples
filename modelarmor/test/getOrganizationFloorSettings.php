0cba49ded4de9d9cb5fd39962abbf6fabf7cdb0a
Harsh Nasit <harsh.nasit@crestdata.ai>
2025-05-30 17:19:04 +0530

    fix(modelarmor): Added snippets for floor settings

---
+++ modelarmor/test/getOrganizationFloorSettings.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright 2025 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\ModelArmor;
+
+use Google\Cloud\TestUtils\TestTrait;
+use PHPUnit\Framework\TestCase;
+
+class getOrganizationFloorSettings extends TestCase
+{
+    use TestTrait;
+
+    private static $organizationId;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$organizationId = getenv("MA_ORG_ID");
+    }
+    public function testGetOrganizationFloorSettings()
+    {
+        $output = $this->runSnippet('get_organization_floor_settings', [
+            self::$organizationId,
+        ]);
+
+        $expectedResponseString = 'Floor settings retrieved successfully:';
+        $this->assertStringContainsString($expectedResponseString, $output);
+    }
+}
---
+++ modelarmor/test/updateProjectFloorSettingsTest.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright 2025 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\ModelArmor;
+
+use Google\Cloud\TestUtils\TestTrait;
+use PHPUnit\Framework\TestCase;
+
+class updateProjectFloorSettingsTest extends TestCase
+{
+    use TestTrait;
+
+    private static $projectId;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$projectId = getenv("GOOGLE_PROJECT_ID");
+    }
+    public function testUpdateProjectFloorSettings()
+    {
+        $output = $this->runSnippet('update_project_floor_settings', [
+            self::$projectId,
+        ]);
+
+        $expectedResponseString = 'Floor settings retrieved successfully:';
+        $this->assertStringContainsString($expectedResponseString, $output);
+    }
+}
