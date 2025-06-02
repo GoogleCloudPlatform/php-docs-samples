ff3d9364fddb6d0e5d9fdf072d17e40893510a8f
Charlotte Y <yeji.yun1225@gmail.com>
2025-06-02 13:42:15 -0700

    chore(PubSub): Adding GCPServiceAccount and AWSRoleArn class variables to pubsubTest

--- pubsub/api/test/pubsubTest.php
+++ pubsub/api/test/pubsubTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright 2016 Google Inc.
  *
@@ -32,6 +33,8 @@ class PubSubTest extends TestCase
     use EventuallyConsistentTestTrait;
 
     private static $eodSubscriptionId;
+    private static $awsRoleArn = 'arn:aws:iam::111111111111:role/fake-role-name';
+    private static $gcpServiceAccount = 'fake-service-account@project.iam.gserviceaccount.com';
 
     public static function setUpBeforeClass(): void
     {
