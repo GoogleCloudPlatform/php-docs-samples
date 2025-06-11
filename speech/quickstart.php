0c7baa1b4ed5b08a61ddbb0eb813f7dba34dae01
Brent Shaffer <betterbrent@google.com>
2025-06-11 16:32:14 -0700

    rewrite speech quickstart for v2

--- speech/quickstart.php
+++ speech/quickstart.php
@@ -16,39 +16,76 @@
  */
 
 # [START speech_quickstart]
-# Includes the autoloader for libraries installed with composer
+// Includes the autoloader for libraries installed with composer
 require __DIR__ . '/vendor/autoload.php';
 
-# Imports the Google Cloud client library
+// Imports the Google Cloud client library
 use Google\Cloud\Speech\V2\Client\SpeechClient;
-use Google\Cloud\Speech\V2\ExplicitDecodingConfig\AudioEncoding;
-use Google\Cloud\Speech\V2\RecognitionAudio;
 use Google\Cloud\Speech\V2\RecognitionConfig;
+use Google\Cloud\Speech\V2\ExplicitDecodingConfig;
+use Google\Cloud\Speech\V2\Recognizer;
 use Google\Cloud\Speech\V2\RecognizeRequest;
+use Google\Cloud\Speech\V2\CreateRecognizerRequest;
+use Google\Cloud\Speech\V2\GetRecognizerRequest;
+use Google\ApiCore\ApiException;
 
-# The name of the audio file to transcribe
-$gcsURI = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
+// The name of the audio file to transcribe
+$gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
 
-# set string as audio content
-$audio = (new RecognitionAudio())
-    ->setUri($gcsURI);
+// Populate these variables with your own values
+$projectId = 'YOUR_PROJECT_ID';
+$recognizerId = 'speech-v2-recognizer-php-quickstart';
+$location = 'global';
 
-# The audio file's encoding, sample rate and language
-$config = new RecognitionConfig([
-    'encoding' => AudioEncoding::LINEAR16,
-    'sample_rate_hertz' => 16000,
-    'language_code' => 'en-US'
-]);
-
-# Instantiates a client
+// Instantiates a client
 $client = new SpeechClient();
 
-# Detects speech in the audio file
+// The name of the recognizer to create
+$recognizerName = $client->recognizerName($projectId, $location, $recognizerId);
+$getRecognizerRequest = (new GetRecognizerRequest())->setName($recognizerName);
+
+try {
+    $recognizer = $client->getRecognizer($getRecognizerRequest);
+} catch (ApiException $e) {
+    if ($e->getStatus() === 'NOT_FOUND') {
+        // If the recognizer does not exist, create it.
+
+        // Create an explicit decoding config because .raw files have no header.
+        $explicitConfig = (new ExplicitDecodingConfig())
+            ->setEncoding(ExplicitDecodingConfig\AudioEncoding::LINEAR16)
+            ->setSampleRateHertz(16000)
+            ->setAudioChannelCount(1); // The brooklyn_bridge audio is single-channel (mono)
+
+        $config = (new RecognitionConfig())
+            ->setLanguageCodes(['en-US'])
+            ->setModel('long') // Or other models like 'telephony', 'medical_dictation'
+            ->setExplicitDecodingConfig($explicitConfig);
+
+        $recognizer = (new Recognizer())
+            ->setName($recognizerName)
+            ->setDefaultRecognitionConfig($config);
+
+        $createRecognizerRequest = (new CreateRecognizerRequest())
+            ->setParent($client->locationName($projectId, $location))
+            ->setRecognizer($recognizer)
+            ->setRecognizerId($recognizerId);
+        $operation = $client->createRecognizer($createRecognizerRequest);
+
+        $operation->pollUntilComplete();
+        $recognizer = $operation->getResult();
+        printf('Created Recognizer: %s' . PHP_EOL, $recognizer->getName());
+    } else {
+        throw $e;
+    }
+}
+
+// Detects speech in the audio file
 $recognizeRequest = (new RecognizeRequest())
-    ->setRecognizer($config);
+    ->setRecognizer($recognizerName)
+    ->setUri($gcsUri);
 $response = $client->recognize($recognizeRequest);
 
-# Print most likely transcription
+// Print most likely transcription
 foreach ($response->getResults() as $result) {
     $alternatives = $result->getAlternatives();
     $mostLikely = $alternatives[0];
