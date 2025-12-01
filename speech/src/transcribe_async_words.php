dad87eb43ae52c0bdb7dc03ba97b1509bcec0edb
Brent Shaffer <betterbrent@google.com>
2025-12-01 16:01:23 -0700

    feat: upgrade speech samples to new surface (#1887)

--- speech/src/transcribe_async_words.php
+++ speech/src/transcribe_async_words.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2016 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,71 +24,97 @@
 namespace Google\Cloud\Samples\Speech;
 
 # [START speech_transcribe_async_word_time_offsets_gcs]
-use Google\Cloud\Speech\V1\SpeechClient;
-use Google\Cloud\Speech\V1\RecognitionAudio;
-use Google\Cloud\Speech\V1\RecognitionConfig;
-use Google\Cloud\Speech\V1\RecognitionConfig\AudioEncoding;
+
+use Google\Cloud\Speech\V2\BatchRecognizeFileMetadata;
+use Google\Cloud\Speech\V2\Client\SpeechClient;
+use Google\Cloud\Speech\V2\BatchRecognizeRequest;
+use Google\Cloud\Speech\V2\ExplicitDecodingConfig;
+use Google\Cloud\Speech\V2\ExplicitDecodingConfig\AudioEncoding;
+use Google\Cloud\Speech\V2\InlineOutputConfig;
+use Google\Cloud\Speech\V2\RecognitionConfig;
+use Google\Cloud\Speech\V2\RecognitionFeatures;
+use Google\Cloud\Speech\V2\RecognitionOutputConfig;
+use Google\Cloud\Speech\V2\SpeakerDiarizationConfig;
 
 /**
- * @param string $audioFile path to an audio file
+ * @param string $projectId The Google Cloud project ID.
+ * @param string $location The location of the recognizer.
+ * @param string $recognizerId The ID of the recognizer to use. The recognizer's model must support
+ *                             diarization (e.g. "chirp_3").
+ * @param string $uri The Cloud Storage object to transcribe (other than global)
+ *                    e.x. gs://cloud-samples-data/speech/brooklyn_bridge.raw
  */
-function transcribe_async_words(string $audioFile)
+function transcribe_async_words(string $projectId, string $location, string $recognizerId, string $uri)
 {
-    // change these variables if necessary
-    $encoding = AudioEncoding::LINEAR16;
-    $sampleRateHertz = 32000;
-    $languageCode = 'en-US';
+    $apiEndpoint = $location === 'global' ? null : sprintf('%s-speech.googleapis.com', $location);
+    $speech = new SpeechClient(['apiEndpoint' => $apiEndpoint]);
+    $recognizerName = SpeechClient::recognizerName($projectId, $location, $recognizerId);
 
-    // When true, time offsets for every word will be included in the response.
-    $enableWordTimeOffsets = true;
+    // When this is enabled, we send all the words from the beginning of the audio.
+    $features = new RecognitionFeatures([
+        'diarization_config' => new SpeakerDiarizationConfig(),
+    ]);
 
-    // get contents of a file into a string
-    $content = file_get_contents($audioFile);
-
-    // set string as audio content
-    $audio = (new RecognitionAudio())
-        ->setContent($content);
-
-    // set config
     $config = (new RecognitionConfig())
-        ->setEncoding($encoding)
-        ->setSampleRateHertz($sampleRateHertz)
-        ->setLanguageCode($languageCode)
-        ->setEnableWordTimeOffsets($enableWordTimeOffsets);
+        ->setFeatures($features)
+        // When running outside the "global" location, you can set the model to "chirp_3" in
+        // RecognitionConfig instead of on the recognizer.
+        // ->setModel('chirp_3')
 
-    // create the speech client
-    $client = new SpeechClient();
+        // Can also use {@see Google\Cloud\Speech\V2\AutoDetectDecodingConfig}
+        // ->setAutoDecodingConfig(new AutoDetectDecodingConfig());
 
-    // create the asyncronous recognize operation
-    $operation = $client->longRunningRecognize($config, $audio);
-    $operation->pollUntilComplete();
+        ->setExplicitDecodingConfig(new ExplicitDecodingConfig([
+            // change these variables if necessary
+            'encoding' => AudioEncoding::LINEAR16,
+            'sample_rate_hertz' => 16000,
+            'audio_channel_count' => 1,
+        ]));
 
-    if ($operation->operationSucceeded()) {
-        $response = $operation->getResult();
+    $outputConfig = (new RecognitionOutputConfig())
+        ->setInlineResponseConfig(new InlineOutputConfig());
 
-        // each result is for a consecutive portion of the audio. iterate
-        // through them to get the transcripts for the entire audio file.
-        foreach ($response->getResults() as $result) {
-            $alternatives = $result->getAlternatives();
-            $mostLikely = $alternatives[0];
-            $transcript = $mostLikely->getTranscript();
-            $confidence = $mostLikely->getConfidence();
-            printf('Transcript: %s' . PHP_EOL, $transcript);
-            printf('Confidence: %s' . PHP_EOL, $confidence);
-            foreach ($mostLikely->getWords() as $wordInfo) {
-                $startTime = $wordInfo->getStartTime();
-                $endTime = $wordInfo->getEndTime();
-                printf('  Word: %s (start: %s, end: %s)' . PHP_EOL,
-                    $wordInfo->getWord(),
-                    $startTime->serializeToJsonString(),
-                    $endTime->serializeToJsonString());
+    $file = new BatchRecognizeFileMetadata();
+    $file->setUri($uri);
+
+    $request = (new BatchRecognizeRequest())
+        ->setRecognizer($recognizerName)
+        ->setConfig($config)
+        ->setFiles([$file])
+        ->setRecognitionOutputConfig($outputConfig);
+
+    try {
+        $operation = $speech->batchRecognize($request);
+        $operation->pollUntilComplete();
+
+        if ($operation->operationSucceeded()) {
+            $response = $operation->getResult();
+            foreach ($response->getResults() as $result) {
+                if ($result->getError()) {
+                    print('Error: '. $result->getError()->getMessage());
+                }
+                // get the most likely transcription
+                $transcript = $result->getInlineResult()->getTranscript();
+                foreach ($transcript->getResults() as $transacriptResult) {
+                    $alternatives = $transacriptResult->getAlternatives();
+                    $mostLikely = $alternatives[0];
+                    foreach ($mostLikely->getWords() as $wordInfo) {
+                        $startTime = $wordInfo->getStartOffset();
+                        $endTime = $wordInfo->getEndOffset();
+                        printf('  Word: %s (start: %s, end: %s)' . PHP_EOL,
+                            $wordInfo->getWord(),
+                            $startTime?->serializeToJsonString(),
+                            $endTime?->serializeToJsonString()
+                        );
+                    }
+                }
             }
+        } else {
+            print_r($operation->getError());
         }
-    } else {
-        print_r($operation->getError());
+    } finally {
+        $speech->close();
     }
-
-    $client->close();
 }
 # [END speech_transcribe_async_word_time_offsets_gcs]
 
